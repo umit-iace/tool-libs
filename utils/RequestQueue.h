c7f8aa41875e0e5a011674dd5322492fad0a9cc6
Jonathan Halmen <jonathan@halmen.org>
2020-04-22 18:01:07 +0200

    update RequestQueue
    * make overfilling queue impossible
    * implement timeout
    * add findRequest method
        * optionally updates timeout time of found request
    * add testcases

--- utils/RequestQueue.h
+++ utils/RequestQueue.h
@@ -10,25 +10,76 @@
  *
  * in case your specific Request class uses dynamically allocated memory,
  * make sure its copy operator and destructor work correctly.
+ *
+ * if you use the findRequest functionality, the `==` operator needs to
+ * be correctly defined as well.
+ *
+ *
+ * in order to use this Queue system, inherit it, and make sure to
+ * implement the `getTime` according to your hardware,
+ * and the `processRequest` function to process your specific request.
+ *
+ * When processing a request is truly done, you must call `endProcess`
+ * in order to progress the queue!
  */
 template<typename Request>
 class RequestQueue {
+public:
+    /**
+     * publicly visible request method
+     */
+    virtual int request(Request r) {
+        return addRequest(r);
+    }
+#ifdef BOOST_TEST_MODULE
 protected:
-    unsigned int FIFOLENGTH;
-    Request *queue;
-    unsigned long long *timeOf = {};
+#else
+private:
+#endif
+    const unsigned int FIFOLENGTH;
+    const unsigned int TIMEOUT;
+    Request *queue = nullptr;
+    unsigned long long *timeOf = nullptr;
     unsigned int iInIndex = 0;
     unsigned int iOutIndex = 0;
-    bool bFirstOp = true;
+    bool bActive = false;
+    bool bFull = false;
 
-public:
+protected:
+    /**
+     * add a request to the queue
+     */
+    int addRequest(Request &r) {
+        int ret = -1;
+        if (!this->bFull) {
+            queue[iInIndex] = r;
+            timeOf[iInIndex] = getTime();
+
+            inc(iInIndex);
+            bFull = iInIndex == iOutIndex && this->bActive;
+            ret = 0;
+        }
+
+        if (!this->bActive) {
+            this->bActive = true;
+            this->processRequest(queue[iOutIndex]);
+        } else if (TIMEOUT && getTime() - timeOf[iOutIndex] > TIMEOUT) {
+            // update timing to reflect restart
+            for (auto index = iOutIndex; index != iInIndex; inc(index)) {
+                timeOf[index] = getTime(); 
+            }
+            // restart
+            this->processRequest(queue[iOutIndex]);
+        }
+        return ret;
+    }
     /**
      * create a new request queue for a specific request type
      *
      * @param length length of queue
      */
-    RequestQueue(unsigned int length) {
-        FIFOLENGTH = length;
+    RequestQueue(unsigned int length, unsigned int timeout) :
+            FIFOLENGTH(length), TIMEOUT(timeout) {
         queue = new Request[length]();
         timeOf = new unsigned long long[length]();
     }
@@ -38,29 +89,28 @@ public:
         delete[] timeOf;
     }
 
-    /**
-     * add a request to the queue
-     *
-     * TODO: overfilling the queue is still possible
-     */
-    void request(Request &r) {
-        while (iInIndex == iOutIndex && !bFirstOp) {
-            // intentionally empty. shouldn't happen
-            asm("nop");
-        }
-
-        queue[iInIndex] = r;
-        timeOf[iInIndex] = getTime();
-
-        iInIndex = (iInIndex + 1) % FIFOLENGTH;
-        if (bFirstOp) {
-            this->initProcess();
-        } else {
-            // TODO timeouts
-        }
+    unsigned int inc(unsigned int &index) {
+        return index = (index + 1) % FIFOLENGTH;
     }
 
-protected:
+    /**
+     * check if request is currently in queue
+     * @param r request to look for
+     * @param updateTime if true, updates found request time to _now_
+     * @return true if the request is found in the queue
+     */
+    bool findRequest(Request &r, bool updateTime = false) {
+        for (auto index = iOutIndex; index != iInIndex; inc(index)) {
+            if (r == queue[index]) {
+                if (updateTime) {
+                    timeOf[index] = getTime();
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
     /**
      * get request at outindex
      */
@@ -75,22 +125,19 @@ protected:
      * removes processed request from list, increments outindex
      */
     void endProcess() {
+        // remove request
         queue[iOutIndex].~Request();
         timeOf[iOutIndex] = 0;
-        iOutIndex = (iOutIndex + 1) % FIFOLENGTH;
-        initProcess();
-    }
 
-    /**
-     * initialize processing the next request in the queue
-     */
-    void initProcess() {
-        if (iOutIndex == iInIndex) { // this was the last request in the queue
-            bFirstOp = true;
-            return;
+        inc(iOutIndex);
+        bFull = false;
+
+        if (iOutIndex == iInIndex) {
+            // queue empty
+            bActive = false;
+        } else {
+            processRequest(queue[iOutIndex]);
         }
-        bFirstOp = false;
-        processRequest(queue[iOutIndex]);
     }
 
     /**
--- utils/RequestQueueTest.cpp
+++ utils/RequestQueueTest.cpp
@@ -35,11 +35,21 @@ public:
         gpio = 0;
         dMem = 0;
     }
+
+    bool operator==(TestRequest &r) {
+        bool ret = true;
+        ret &= this->dMem == r.dMem;
+        ret &= this->sTest == r.sTest;
+        ret &= this->pin == r.pin;
+        ret &= this->gpio == r.gpio;
+        return ret;
+    }
 };
 
 class TestFifo: public RequestQueue<TestRequest> {
+    bool bStop = false;
 public:
-    TestFifo(unsigned int l): RequestQueue(l) {}
+    TestFifo(unsigned int l): RequestQueue(l, 2) {}
 
     int getIn() {
         return this->iInIndex;
@@ -49,14 +59,19 @@ public:
         return this->iOutIndex;
     }
 
-    bool getFirstOp() {
-        return this->bFirstOp;
+    bool isActive() {
+        return this->bActive;
     }
 
     void processRequest(TestRequest &r) {
         printf("Pin: %d\n\t on port: %X\n"
                "mem: %f\n", r.pin, r.gpio, r.dMem);
-        endProcess();
+        if (!bStop) {
+            printf("processed.\n");
+            endProcess();
+        } else {
+            printf("not processed.\n");
+        }
     }
 
     TestRequest *getQueue(){
@@ -64,12 +79,27 @@ public:
     }
 
     unsigned long getTime() override {
-        return 0;
+        static unsigned long time = 1;
+        return time++;
+    }
+
+    void stop() {
+        printf("stopped\n");
+        this->bStop = true;
+    }
+    
+    void start() {
+        printf("started\n");
+        this->bStop = false;
+    }
+    bool find(TestRequest &r) {
+        return findRequest(r);
     }
 };
 
 
-BOOST_AUTO_TEST_CASE( singleRequestTest ) {
+BOOST_AUTO_TEST_CASE( emptyRequestTest ) {
+        printf("\n\nemptyRequestTest\n\n");
         const int fifosize = 10;
         const int testsize = 6;
         TestFifo fifo(fifosize);
@@ -86,7 +116,37 @@ BOOST_AUTO_TEST_CASE( singleRequestTest ) {
 
         BOOST_CHECK_EQUAL(fifo.getIn(), testsize);
 
-        BOOST_CHECK_EQUAL(fifo.getFirstOp(), true);
+        BOOST_CHECK_EQUAL(fifo.isActive(), false);
 
         BOOST_CHECK_EQUAL(memcmp(fifo.getQueue(), zeros, fifosize*sizeof(TestRequest)), 0);
 }
+
+BOOST_AUTO_TEST_CASE(overfullTest) {
+        printf("\n\noverfullTest\n\n");
+        int fifosize = 5;
+        int testsize = 10;
+        TestFifo fifo(fifosize);
+        fifo.stop();
+        for (int i = 0; i < testsize; ++i) {
+            auto R = TestRequest(i, 0x11f7a345+i);
+            BOOST_CHECK_EQUAL(fifo.request(R), i < fifosize ? 0 : -1);
+        }
+        fifo.start();
+        auto r = TestRequest(121, 0xbabeface);
+        BOOST_CHECK_EQUAL(fifo.request(r), -1);
+        BOOST_CHECK_EQUAL(fifo.getIn(), 0);
+        BOOST_CHECK_EQUAL(fifo.request(r), 0);
+        BOOST_CHECK_EQUAL(fifo.getIn(), 1);
+}
+
+BOOST_AUTO_TEST_CASE(findRequestTest) {
+        printf("\n\nfindRequestTest\n\n");
+        TestFifo fifo(2);
+        auto R = TestRequest(0, 0x11f7a345);
+        fifo.request(R);
+        BOOST_CHECK_EQUAL(fifo.find(R), false);
+        fifo.stop();
+        fifo.request(R);
+        BOOST_CHECK_EQUAL(fifo.find(R), true);
+}
+
