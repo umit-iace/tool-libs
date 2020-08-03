78bf974bf209914d266d127c6a23f59fdbab6d61
Jonathan Halmen <jonathan@halmen.org>
2020-08-04 00:58:57 +0200

    reimplement pwm class
    to be used with HardwareTimer
    
    should be a lot more pleasant to use now

--- stm/pwm.h
+++ stm/pwm.h
@@ -8,55 +8,35 @@
 #include "stm/hal.h"
 
 /**
- * @brief Template class for hardware based PWM derivations
+ * @brief Template class for hardware based PWM outputs
  */
 class HardwarePWM {
 public:
     /**
      * set pwm in percent
      * @param perc [0..1]
-     * @param chan Timer channel (TIM_CHANNEL_x)
      */
-    void pwm(double perc, uint32_t chan) {
-        __HAL_TIM_SET_COMPARE(&hPWMTim, chan, perc * period);
+    void pwm(double perc) {
+        __HAL_TIM_SET_COMPARE(hTim, channel, perc * period);
     }
 
     /**
      * Initialize Timer Hardware and PWM pins
      *
-     * @param dTim TIMx
-     * @param prescaler 16bit value
-     * @param period 16bit value
-     *
-     * Timer counts with frequency of `TIM_periph / prescaler` from 0 to `period`
-     */
-    HardwarePWM(TIM_TypeDef *dTim, uint32_t prescaler, uint32_t period) : period(period) {
-        // clock config
-        TIM_MasterConfigTypeDef sMasterConfig = {};
-
-        hPWMTim.Instance = dTim;
-        hPWMTim.Init.Prescaler = prescaler;
-        hPWMTim.Init.CounterMode = TIM_COUNTERMODE_UP;
-        hPWMTim.Init.Period = period;
-        hPWMTim.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-        hPWMTim.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-        while (HAL_TIM_PWM_Init(&hPWMTim) != HAL_OK);
-
-        sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-        sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-        while (HAL_TIMEx_MasterConfigSynchronization(&hPWMTim, &sMasterConfig) != HAL_OK);
-    }
-
-    /**
-     * enable PWM output on given timer channel
+     * @param hTim handle of configured HardwareTimer
      * @param iPin pin number of PWM output
      * @param gpioPort pin port of PWM output
      * @param iAlternate alternate function for PWM output
      * @param chan timer channel number (TIM_CHANNEL_x)
      */
-    void startChannel(uint32_t iPin, GPIO_TypeDef *gpioPort, uint8_t iAlternate, uint32_t chan)
-    {
-        // init pins
+    HardwarePWM(TIM_HandleTypeDef *hTim,
+                uint32_t iPin, GPIO_TypeDef *gpioPort,
+                uint8_t iAlternate, uint32_t chan) :
+            hTim(hTim), channel(chan), period(hTim->Init.Period) {
+        // pwm config of timer
+        while (HAL_TIM_PWM_Init(hTim) != HAL_OK);
+
+        // pin config
         GPIO_InitTypeDef GPIO_InitStruct = {};
         GPIO_InitStruct.Pin = iPin;
         GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
@@ -65,19 +45,22 @@ public:
         GPIO_InitStruct.Alternate = iAlternate;
         HAL_GPIO_Init(gpioPort, &GPIO_InitStruct);
 
+        // channel config
         TIM_OC_InitTypeDef sConfigOC = {};
         sConfigOC.OCMode = TIM_OCMODE_PWM1;
         sConfigOC.Pulse = 0;
         sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
         sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-        while (HAL_TIM_PWM_ConfigChannel(&hPWMTim, &sConfigOC, chan) != HAL_OK);
+        while (HAL_TIM_PWM_ConfigChannel(hTim, &sConfigOC, chan) != HAL_OK);
 
-        while (HAL_TIM_PWM_Start(&hPWMTim, chan) != HAL_OK);
+        __HAL_TIM_SET_COMPARE(hTim, chan, 0);
+        while (HAL_TIM_PWM_Start(hTim, chan) != HAL_OK);
     }
 
 private:
     //\cond false
-    TIM_HandleTypeDef hPWMTim = {};
+    TIM_HandleTypeDef *hTim = nullptr;
+    uint32_t channel = 0;
     uint32_t period = 0;
     //\endcond
 };
