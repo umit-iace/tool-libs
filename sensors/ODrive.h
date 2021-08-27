6987d57a82e27aabfbb2bdd41c00c1c28551aaa8
Florian Dengg <florian@ummerland.com>
2021-08-27 13:09:27 +0200

    Added ODrie Driver

---
+++ sensors/ODrive.h
@@ -0,0 +1,47 @@
+/** @file ODrive.h
+ *
+ * Copyright (c) 2021 IACE
+ */
+
+#ifndef RIG_ODRIVE_H
+#define RIG_ODRIVE_H
+
+#include "stm/uart.h"
+
+class ODrive {
+    
+public:
+    
+    /**
+     * Actuators
+     */
+    enum Actuator {
+        M0 = 0,
+        M1 = 1
+    };
+    
+    /**
+     * initialize the ODrive abstraction
+     */
+    ODrive(HardwareUART *uartInstance)
+    {
+        instance = uartInstance;
+    }
+    
+    /**
+     * Set motor speed by actuator number (M0 and M1) and the angular velocity in revolutions per minute.
+     */
+    void setSpeed(Actuator actuator, uint32_t revPerMin) {
+        UARTRequest *urq = new UARTRequest(UARTbuffer, nullptr, strlen((char*)UARTbuffer), nullptr);
+        instance->request(*urq);
+    }
+    
+private:
+    
+    HardwareUART *instance = nullptr;
+    
+    
+};
+
+
+#endif //RIG_ODRIVE_H
