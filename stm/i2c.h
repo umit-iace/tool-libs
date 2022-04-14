520c4d42dec5592b00dd18b7843c6145ce19b90c
Jonathan Halmen <jonathan@halmen.org>
2022-04-14 12:00:45 +0200

    i2c: simplify constructor, rm pins
    also removes the singleton implementation from it, as it's not needed
    anymore if the i2cdevices receive a pointer to them (casted to
    RequestQueue<I2CRequest>*)

--- stm/i2c.h
+++ stm/i2c.h
@@ -5,13 +5,8 @@
 #ifndef STM_I2C_H
 #define STM_I2C_H
 
-#include "stm/gpio.h"
 #include "stm/hal.h"
 #include "utils/RequestQueue.h"
-
-extern "C" {
-I2C_HandleTypeDef hHWI2C;
-};
 
 /**
  * generic I2C device wrapper.
@@ -121,13 +116,6 @@ public:
         return HAL_GetTick();
     }
 
-    static HardwareI2C *master() {
-        if (!pThis) {
-            pThis = new HardwareI2C();
-        }
-        return pThis;
-    }
-
 private:
     /**
      * override virtual RequestQueue function
@@ -138,20 +126,20 @@ private:
     void rqBegin(I2CRequest *rq) override {
         switch (rq->type) {
             case I2CRequest::I2C_DIRECT_READ:
-                HAL_I2C_Master_Receive_IT(&hI2C, rq->dev->addr << 1,
+                HAL_I2C_Master_Receive_IT(&handle, rq->dev->addr << 1,
                                           rq->pData, rq->dataLen);
                 break;
             case I2CRequest::I2C_DIRECT_WRITE:
-                HAL_I2C_Master_Transmit_IT(&hI2C, rq->dev->addr << 1,
+                HAL_I2C_Master_Transmit_IT(&handle, rq->dev->addr << 1,
                                            rq->pData, rq->dataLen);
                 break;
             case I2CRequest::I2C_MEM_READ:
-                HAL_I2C_Mem_Read_IT(&hI2C, rq->dev->addr << 1,
+                HAL_I2C_Mem_Read_IT(&handle, rq->dev->addr << 1,
                                     rq->memAddress, I2C_MEMADD_SIZE_8BIT,
                                     rq->pData, rq->dataLen);
                 break;
             case I2CRequest::I2C_MEM_WRITE:
-                HAL_I2C_Mem_Write_IT(&hI2C, rq->dev->addr << 1,
+                HAL_I2C_Mem_Write_IT(&handle, rq->dev->addr << 1,
                                      rq->memAddress, I2C_MEMADD_SIZE_8BIT,
                                      rq->pData, rq->dataLen);
                 break;
@@ -162,7 +150,7 @@ private:
      * timeout
      */
     void rqTimeout(I2CRequest *rq) override {
-        HAL_I2C_Master_Abort_IT(&hI2C, rq->dev->addr << 1);
+        HAL_I2C_Master_Abort_IT(&handle, rq->dev->addr << 1);
         rqEnd();
     }
 
@@ -187,61 +175,47 @@ private:
      static void errorCallback(I2C_HandleTypeDef *hI2C) {
          pThis->rqEnd();
      }
+    //\cond false
+    inline static HardwareI2C *pThis = nullptr;
+    //\endcond
 
-    HardwareI2C() : RequestQueue(50, HW_I2C_TIMEOUT)  {
-        AFIO(HW_I2C_SDA_PIN, HW_I2C_SDA_PORT, HW_I2C_SDA_ALTERNATE,
-            GPIO_NOPULL, GPIO_SPEED_FREQ_HIGH, GPIO_MODE_AF_OD);
-        AFIO(HW_I2C_SCL_PIN, HW_I2C_SCL_PORT, HW_I2C_SCL_ALTERNATE,
-            GPIO_NOPULL, GPIO_SPEED_FREQ_HIGH, GPIO_MODE_AF_OD);
-        this->config(HW_I2C, HW_I2C_BAUD);
-    }
-
-    void config(I2C_TypeDef *i2c, uint32_t baud) {
-        hI2C.Instance = i2c;
+public:
+    HardwareI2C(I2C_TypeDef *i2c, uint32_t baud, uint32_t timeout) :
+            RequestQueue(50, timeout)
+    {
+        handle.Instance = i2c;
+        handle.Init = {
 #if defined(STM32F407xx)
-        hI2C.Init.ClockSpeed = baud;
-        hI2C.Init.DutyCycle = I2C_DUTYCYCLE_2;
+            .ClockSpeed = baud,
+            .DutyCycle = I2C_DUTYCYCLE_2,
 #elif defined(STM32F767xx)
-        // shoot me now
-        // hardcoded to 400kHz if i2cclk 216MHz
-        hI2C.Init.Timing = 0x7 << 28 | // presc
+            // shoot me now
+            // hardcoded to 400kHz if i2cclk 216MHz
+            .Timing = 0x7 << 28 | // presc
                             4 << 20 | // data setup time
                             4 << 16 | // data hold time
                             21 << 8 | // high period
-                            40 << 0; // low period
+                            40 << 0, // low period
 #endif
-        hI2C.Init.OwnAddress1 = 0;
-        hI2C.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-        hI2C.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-        hI2C.Init.OwnAddress2 = 0;
-        hI2C.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-        hI2C.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-        while (HAL_I2C_Init(&hI2C) != HAL_OK);
+            .OwnAddress1 = 0,
+            .AddressingMode = I2C_ADDRESSINGMODE_7BIT,
+            .DualAddressMode = I2C_DUALADDRESS_DISABLE,
+            .OwnAddress2 = 0,
+            .GeneralCallMode = I2C_GENERALCALL_DISABLE,
+            .NoStretchMode = I2C_NOSTRETCH_DISABLE,
+        };
+        while (HAL_I2C_Init(&handle) != HAL_OK);
 
-        HAL_I2C_RegisterCallback(&hI2C, HAL_I2C_MEM_TX_COMPLETE_CB_ID, transferComplete);
-        HAL_I2C_RegisterCallback(&hI2C, HAL_I2C_MEM_RX_COMPLETE_CB_ID, transferComplete);
-        HAL_I2C_RegisterCallback(&hI2C, HAL_I2C_MASTER_TX_COMPLETE_CB_ID, transferComplete);
-        HAL_I2C_RegisterCallback(&hI2C, HAL_I2C_MASTER_RX_COMPLETE_CB_ID, transferComplete);
+        HAL_I2C_RegisterCallback(&handle, HAL_I2C_MEM_TX_COMPLETE_CB_ID, transferComplete);
+        HAL_I2C_RegisterCallback(&handle, HAL_I2C_MEM_RX_COMPLETE_CB_ID, transferComplete);
+        HAL_I2C_RegisterCallback(&handle, HAL_I2C_MASTER_TX_COMPLETE_CB_ID, transferComplete);
+        HAL_I2C_RegisterCallback(&handle, HAL_I2C_MASTER_RX_COMPLETE_CB_ID, transferComplete);
 
-        HAL_I2C_RegisterCallback(&hI2C, HAL_I2C_ERROR_CB_ID, errorCallback);
+        HAL_I2C_RegisterCallback(&handle, HAL_I2C_ERROR_CB_ID, errorCallback);
 
-        HAL_NVIC_SetPriority(I2C1_ER_IRQn, HW_I2C_IT_PRIO);
-        HAL_NVIC_EnableIRQ(I2C1_ER_IRQn);
-        HAL_NVIC_SetPriority(I2C1_EV_IRQn, HW_I2C_IT_PRIO);
-        HAL_NVIC_EnableIRQ(I2C1_EV_IRQn);
+        pThis = this;
     }
 
-    //\cond false
-    inline static HardwareI2C *pThis = nullptr;
-    I2C_HandleTypeDef &hI2C = hHWI2C;
-    //\endcond
+    I2C_HandleTypeDef handle{};
 };
-
-extern "C" void I2C1_EV_IRQHandler() {
-    HAL_I2C_EV_IRQHandler(&hHWI2C);
-}
-
-extern "C" void I2C1_ER_IRQHandler() {
-    HAL_I2C_ER_IRQHandler(&hHWI2C);
-}
 #endif //STM_I2C_H
