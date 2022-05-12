9c3cb1769cdfbc60a4decfdc2fbfd984baaee420
Jonathan Halmen <jonathan@halmen.org>
2022-05-13 01:55:20 +0200

    AS5145: make used spi bus explicit
    also some fixes, and nice-to-have's

--- sensors/AS5145.h
+++ sensors/AS5145.h
@@ -13,6 +13,7 @@
 class AS5145 : ChipSelect {
 private:
     ///\cond false
+    RequestQueue<SPIRequest> *bus;
     // sensor data struct. lsb to msb order.
     struct __packed SensorData {
         uint8_t EVEN:1;
@@ -21,7 +22,7 @@ private:
         uint8_t LIN:1;
         uint8_t COF:1;
         uint8_t OCF:1;
-        uint16_t POS:12;
+        int16_t POS:12;
         uint8_t :1;
     } *sensor = nullptr;
     const unsigned short NUMBITS = 19;  // number of bits in sensor data struct
@@ -33,11 +34,12 @@ private:
 public:
     /**
      * Constructor
-     * @param pin GPIO pin of chip select line
-     * @param port GPIO port of chip select line
-     * @param n     number of sensors attached to daisy-chain
+     * @param bus SPI bus
+     * @param cs ChipSelect pin
+     * @param n number of sensors attached to daisy-chain
      */
-    AS5145(uint32_t pin, GPIO_TypeDef *port, int n) : ChipSelect(pin, port),
+    AS5145(RequestQueue<SPIRequest> *bus, DIO cs, int n) : ChipSelect(cs),
+            bus(bus),
             BUFLEN((n * NUMBITS) / 8UL + (n * NUMBITS % 8 ? 1 : 0)) // calculate bytes needed for n sensors
             {
         buffer = new uint8_t[BUFLEN]();
@@ -49,7 +51,7 @@ public:
      * @param i index of sensor in chain
      * @return measured sensor angle
      */
-    double getAngle(int i = 0) {
+    int16_t getVal(int i = 0) {
         return sensor[i].POS;
     }
 
@@ -57,13 +59,13 @@ public:
      * request measurement of sensor data
      */
     void sense() {
-        HardwareSPI::master()->request(new SPIRequest(
+        bus->request(new SPIRequest{
                 this,
                 SPIRequest::MISO,
                 nullptr,
                 buffer,
                 BUFLEN,
-                (void *)true)
+                (void *)true}
         );
     }
 
@@ -74,7 +76,7 @@ public:
      * copy data from buffer into struct.
      */
     void callback(void *cbData) override {
-        bitwisecopy((uint8_t *)sensor, NUMBITS, sizeof(*sensor), buffer, num);
+        bitwisecopy((uint8_t *)sensor, buffer, NUMBITS, num);
     }
     ///\endcond
 };
