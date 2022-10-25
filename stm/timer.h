66522aa7f4756d1c6f2408aeb7f5c65c7d19cf9b
Jonathan Halmen <jonathan@halmen.org>
2022-10-25 16:30:45 +0200

    tim: wrap HAL irq handler and expose HAL handle

--- stm/timer.h
+++ stm/timer.h
@@ -20,25 +20,22 @@ public:
      * @param period value to write into period register
      */
     HardwareTimer(TIM_TypeDef *dTim, uint32_t prescaler, uint32_t period) {
-        this->hTim.Instance = dTim;
         TIM_MasterConfigTypeDef sMasterConfig = {};
-        hTim.Init.Prescaler = prescaler;
-        hTim.Init.CounterMode = TIM_COUNTERMODE_UP;
-        hTim.Init.Period = period;
-        hTim.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-        hTim.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-        while (HAL_TIM_Base_Init(&hTim) != HAL_OK);
+        handle = {
+            .Instance = dTim,
+            .Init = {
+                .Prescaler = prescaler,
+                .CounterMode = TIM_COUNTERMODE_UP,
+                .Period = period,
+                .ClockDivision = TIM_CLOCKDIVISION_DIV1,
+                .AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE,
+            },
+        };
+        while (HAL_TIM_Base_Init(&handle) != HAL_OK);
 
         sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
         sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-        while (HAL_TIMEx_MasterConfigSynchronization(&hTim, &sMasterConfig) != HAL_OK);
-    }
-
-    /**
-     * return HAL handle to timer object
-     */
-    TIM_HandleTypeDef *handle() {
-        return &hTim;
+        while (HAL_TIMEx_MasterConfigSynchronization(&handle, &sMasterConfig) != HAL_OK);
     }
 
     /**
@@ -49,20 +46,22 @@ public:
      * @param sub priority
      */
     void configCallback(void (*callback)(TIM_HandleTypeDef *)) {
-        HAL_TIM_RegisterCallback(&hTim, HAL_TIM_PERIOD_ELAPSED_CB_ID, callback);
+        HAL_TIM_RegisterCallback(&handle, HAL_TIM_PERIOD_ELAPSED_CB_ID, callback);
     }
 
     /**
      * start timer in interrupt mode
      */
     void start() {
-        while (HAL_TIM_Base_Start_IT(&hTim) != HAL_OK);
+        while (HAL_TIM_Base_Start_IT(&handle) != HAL_OK);
     }
 
-private:
-    //\cond false
-    TIM_HandleTypeDef hTim = {};
-    //\endcond
+    void irqHandler() {
+        HAL_TIM_IRQHandler(&handle);
+    }
+
+    /** HAL handle, for use with unwrapped Timer capabilities */
+    TIM_HandleTypeDef handle = {};
 };
 
 #endif //STM_TIMER_H
