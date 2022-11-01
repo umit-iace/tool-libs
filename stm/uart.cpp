291d19d74f2b4349c6ca92ea2daf18433a49ee6a
Jonathan Halmen <jonathan@halmen.org>
2022-11-01 21:27:17 +0100

    uart: try exposing queues for rx and tx
    splits uart in header and implementation file for now

---
+++ stm/uart.cpp
@@ -0,0 +1,97 @@
+/** @file uart.cpp
+ *
+ * Copyright (c) 2022 IACE
+ */
+#include "uart.h"
+#include "stm/hal.h"
+#include "stm/gpio.h"
+
+/* HAL glue */
+void _rxcallback(UART_HandleTypeDef *handle, uint16_t nbs) {
+    HardwareUART::reg.from(handle)->rxcallback(nbs);
+}
+void _txcallback(UART_HandleTypeDef *handle) {
+    HardwareUART::reg.from(handle)->txcallback();
+}
+
+/**
+ * Initialize the peripheral
+ *
+ * make sure to also initialize the corresponding rx/tx AFIO pins
+ *
+ * @param dUsart definition of used UART
+ * @param iBaudRate baud rate
+ */
+HardwareUART::HardwareUART(USART_TypeDef *dUsart, uint32_t iBaudRate) :
+    RequestQueue<UART_TX>(10, 0),
+    RequestQueue<UART_RX>(10, 0)
+{
+    reg.reg(this, &handle);
+    handle.Instance = dUsart;
+    handle.Init = {
+            .BaudRate = iBaudRate,
+            .WordLength = UART_WORDLENGTH_8B,
+            .StopBits = UART_STOPBITS_1,
+            .Parity = UART_PARITY_NONE,
+            .Mode = UART_MODE_TX_RX,
+            .HwFlowCtl = UART_HWCONTROL_NONE,
+            .OverSampling = UART_OVERSAMPLING_16,
+    };
+    while (HAL_UART_Init(&handle) != HAL_OK);
+    HAL_UART_RegisterRxEventCallback(&handle, _rxcallback);
+    HAL_UART_RegisterCallback(&handle, HAL_UART_TX_COMPLETE_CB_ID, _txcallback);
+}
+
+void HardwareUART::rxcallback(uint16_t nbs) {
+    bool rqend = false;
+    uint8_t c= nbs+'0';
+    uint8_t nl = '\n';
+    request(new UART_TX(&nl, 1));
+    request(new UART_TX(&c, 1));
+    request(new UART_TX(&nl, 1));
+    auto r = RequestQueue<UART_RX>::rqCurrent();
+    r->buf += nbs;
+    r->len -= nbs;
+    /* if (r->buf + r->len == handle.) { */
+    if (handle.RxXferCount == 0) {
+        rqend = true;
+        auto led = DIO(GPIO_PIN_13, GPIOC);
+        led.set(false);
+    } else {
+        assert(r->len);
+        rqBegin(r);
+    }
+    if (r->bytewise) {
+        do {
+            r->bytewise(r->buf[-(--nbs+1)]);
+        } while (nbs);
+    }
+    if (rqend) {
+        RequestQueue<UART_RX>::rqEnd();
+    }
+}
+void HardwareUART::txcallback() {
+    this->RequestQueue<UART_TX>::rqEnd();
+}
+
+short HardwareUART::request(UART_TX *r) {
+    return this->RequestQueue<UART_TX>::request(r);
+}
+short HardwareUART::request(UART_RX *r) {
+    return this->RequestQueue<UART_RX>::request(r);
+}
+
+void HardwareUART::irqHandler() {
+    HAL_UART_IRQHandler(&handle);
+}
+
+void HardwareUART::rqBegin(UART_TX *r) {
+    HAL_UART_Transmit_IT(&handle, r->buf, r->len);
+}
+void HardwareUART::rqBegin(UART_RX *r) {
+    HAL_UARTEx_ReceiveToIdle_IT(&handle, r->buf, r->len);
+}
+/* we don't use these */
+void HardwareUART::rqTimeout(UART_TX *r) { /* not implemented */ }
+void HardwareUART::rqTimeout(UART_RX *r) { /* not implemented */ }
+unsigned long HardwareUART::getTime() { return 0; }
--- stm/uart.h
+++ stm/uart.h
@@ -6,12 +6,35 @@
 #define STM_UART_H
 
 #include "stm/hal.h"
+#include "stm/registry.h"
+#include "utils/RequestQueue.h"
+#include <cstring>
 
+struct UART_TX {
+    static constexpr unsigned int SZ = 512;
+    unsigned char buf[SZ];
+    unsigned short len;
+    UART_TX() {len = 0;}
+    UART_TX(unsigned char *src, unsigned short len) : len(len) {
+        memcpy(buf, src, len);
+    }
+};
+struct UART_RX {
+    UART_RX() {buf = nullptr; len = 0;}
+    UART_RX(unsigned char *dst, unsigned short len) : buf(dst), len(len) {}
+    UART_RX(unsigned char *dst,
+            unsigned short len,
+            void (*bw)(uint8_t c)) : buf(dst), len(len), bytewise(bw) {}
+    unsigned char *buf;
+    unsigned short len;
+    void (*bytewise)(uint8_t c) = nullptr;
+};
 /**
  * @brief Template class for hardware based UART derivations
  */
-class HardwareUART {
+class HardwareUART : public RequestQueue<UART_TX>, public RequestQueue<UART_RX>{
 public:
+    inline static Registry<HardwareUART, UART_HandleTypeDef, 8> reg;
     /**
      * Initialize the peripheral
      *
@@ -20,21 +43,23 @@ public:
      * @param dUsart definition of used UART
      * @param iBaudRate baud rate
      */
-    HardwareUART(USART_TypeDef *dUsart, uint32_t iBaudRate) {
-        handle.Instance = dUsart;
-        handle.Init = {
-                .BaudRate = iBaudRate,
-                .WordLength = UART_WORDLENGTH_8B,
-                .StopBits = UART_STOPBITS_1,
-                .Parity = UART_PARITY_NONE,
-                .Mode = UART_MODE_TX_RX,
-                .HwFlowCtl = UART_HWCONTROL_NONE,
-                .OverSampling = UART_OVERSAMPLING_16,
-        };
-        while (HAL_UART_Init(&handle) != HAL_OK);
-    }
-
+    HardwareUART(USART_TypeDef *dUsart, uint32_t iBaudRate);
+    void rxcallback(uint16_t nbs);
+    void txcallback();
+    /** interrupt handler
+     *
+     * call this directly in interrupt
+     */
+    void irqHandler();
+    /** HAL handle for use with unwrapped UART capabilities */
     UART_HandleTypeDef handle{};
+    /* queue defs */
+    short request(UART_TX *r) override;
+    short request(UART_RX *r) override;
+    void rqBegin(UART_TX *r) override;
+    void rqBegin(UART_RX *r) override;
+    void rqTimeout(UART_TX *r) override;
+    void rqTimeout(UART_RX *r) override;
+    unsigned long getTime() override;
 };
-
 #endif //STM_UART_H
