ea02a586a37677ae3e635dcaf8601b73d654a37c
Jonathan Halmen <jonathan@halmen.org>
2023-01-23 14:25:21 +0100

    add queue tests

---
+++ tests/Queue.cpp
@@ -0,0 +1,56 @@
+#include <cassert>
+#include <cstdio>
+#define DEBUGLOG(...) fprintf(__VA_ARGS__)
+#include "utils/Queue.h"
+#include "utils/Buffer.h"
+#include <queue>
+#include <iostream>
+using namespace std;
+struct data {
+    double d;
+    data(double d): d(d) {
+        cout << "constr " << d << endl;
+    }
+    ~data() {
+        cout << "destr " << d << endl;
+        d = 0;
+    }
+    operator double() { return d; }
+};
+void strdat(){
+    /* queue<struct data> q; */
+    Queue<struct data, 3> q;
+    q.push(4.);
+    q.push(8.);
+    q.push(3.14);
+    while (!q.empty()) {
+        cout << q.front() << " ";// << q.back() << endl;
+        /* q.pop(); */
+        cout << q.pop() << " ";
+    }
+    cout << endl;
+}
+void buf() {
+    Buffer b{5};
+    b.append('a').append('b').append('c').append('d').append('e');
+    Queue<Buffer, 3> q;
+    q.push(std::move(b));
+    Buffer c = q.pop();
+    for (size_t ix = 0; ix < c.len; ++ix) {
+        cout << c.at(ix) << ' ';
+    }
+    cout << endl;
+}
+void roundabout() {
+    Queue<double, 4> q;
+    for (size_t i = 1; i < 20; ++i) {
+        q.push(10./i);
+        cout << q.pop() << " ";
+    }
+    cout << endl;
+}
+int main() {
+    strdat();
+    buf();
+    roundabout();
+}
