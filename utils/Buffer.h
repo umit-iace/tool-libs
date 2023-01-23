1a43c12833a9145c4a1d070d4d6c3344c9ccccbc
Jonathan Halmen <jonathan@halmen.org>
2023-01-23 10:40:15 +0100

    Buffer: implement copy assignment operator
    and restructure

--- utils/Buffer.h
+++ utils/Buffer.h
@@ -6,38 +6,8 @@
 struct Buffer {
     uint8_t *payload;
     size_t len, size;
-    Buffer(size_t sz) : payload{new uint8_t[sz]()}, len(0), size(sz) {
-        /* fprintf(stderr, "Buffer new payload: %p\n", payload); */
-    }
-    /** copy constructor */
-    Buffer(const Buffer &b) : payload{new uint8_t[b.size]()}, len(b.len), size(b.size) {
-        /* fprintf(stderr, "Buffer cp constr\n"); */
-        memcpy(payload, b.payload, len);
-        /* fprintf(stderr, "Buffer cpy payload: %p\n", payload); */
-    }
-    ~Buffer() {
-        /* fprintf(stderr, "Buffer del payload: %p\n", payload); */
-        delete[] payload;
-    }
-    /** move constructor */
-    Buffer(Buffer &&b) : payload(b.payload), len(b.len), size(b.size) {
-        /* fprintf(stderr, "Buffer mv constr\n"); */
-        b.payload = 0;
-        b.len = 0;
-        b.size = 0;
-    }
-    /** move operator */
-    Buffer& operator=(Buffer &&b) {
-        delete[] payload;
-        /* fprintf(stderr, "Buffer mv from to: %p %p\n", b.payload, payload); */
-        payload = b.payload;
-        len = b.len;
-        size = b.size;
-        b.len = 0;
-        b.size = 0;
-        b.payload = nullptr;
-        return *this;
-    }
+
+    /** access */
     uint8_t& at(size_t ix) {
         assert(ix < len);
         return payload[ix];
@@ -46,8 +16,60 @@ struct Buffer {
         assert(ix < size);
         return payload[ix];
     }
-    void append(uint8_t b) {
+    Buffer& append(uint8_t b) {
         assert(len < size);
         payload[len++] = b;
+        return *this;
+    }
+    /* Rule of Five */
+    /** destructor */
+    ~Buffer() {
+        /* fprintf(stderr, "Buffer del payload: %p\n", payload); */
+        delete[] payload; len = 0; size = 0;
+    }
+    /** constructor */
+    Buffer(size_t sz) : payload{new uint8_t[sz]()}, len(0), size(sz) {
+        /* fprintf(stderr, "Buffer new payload: %p\n", payload); */
+    }
+    /** copy constructor */
+    Buffer(const Buffer &b) : payload{new uint8_t[b.size]()}, len(b.len), size(b.size) {
+        /* fprintf(stderr, "Buffer cp constr\n"); */
+        memcpy(payload, b.payload, len);
+        /* fprintf(stderr, "Buffer cp constr from: %p to: %p\n", b.payload, payload); */
+    }
+    /** copy assignment operator */
+    Buffer& operator=(const Buffer &b) {
+        if (this == &b) return *this; // copy to self
+        if (!size && size != b.size) { // necessary to realloc
+            delete[] payload;
+            payload = new uint8_t[b.size]();
+            size = b.size;
+        }
+        assert(payload != nullptr);
+        assert(size > b.len);
+        /* fprintf(stderr, "Buffer cp from to: %p %p\n", b.payload, payload); */
+        memcpy(payload, b.payload, b.len);
+        len = b.len;
+        return *this;
+    }
+    /** move constructor */
+    Buffer(Buffer &&b) noexcept : payload(b.payload), len(b.len), size(b.size) {
+        /* fprintf(stderr, "Buffer mv constr from: %p to: %p\n", b.payload, payload); */
+        b.payload = 0;
+        b.len = 0;
+        b.size = 0;
+    }
+    /** move assignment operator */
+    Buffer& operator=(Buffer &&b) noexcept {
+        if (this == &b) return *this; // move to self
+        /* fprintf(stderr, "Buffer mv from to: %p %p\n", b.payload, payload); */
+        delete[] payload;
+        payload = b.payload;
+        len = b.len;
+        size = b.size;
+        b.len = 0;
+        b.size = 0;
+        b.payload = nullptr;
+        return *this;
     }
 };
