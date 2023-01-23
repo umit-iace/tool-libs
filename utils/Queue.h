080d95544de6e3e6f1fcf160c39ee0237d28037a
Jonathan Halmen <jonathan@halmen.org>
2023-01-23 14:24:41 +0100

    rework Queue
    includes copy and move semantics

--- utils/Queue.h
+++ utils/Queue.h
@@ -1,14 +1,83 @@
 #pragma once
+#include <cstddef>
+#include <cstdint>
+#include <utility>
+#ifndef DEBUGLOG
+#define DEBUGLOG(...)
+#define UNDEF
+#endif
 
 /**
  * simple Array backed circle buffer queue to be used as FIFO
- *
- * XXX: currently only works as used in Min.h ===> only actually marks
- * the last element as done and moves on
  */
 template <typename T, int sz>
-struct Queue {
-    T q[sz];
+class Queue {
+public:
+    /**
+     * copy element into queue
+     */
+    Queue& push(const T &val) {
+        DEBUGLOG(stderr, "copying ..\n");
+        return push(std::move(T{val}));
+    }
+    /**
+     * move element into queue
+     */
+    Queue& push(T &&val) {
+        DEBUGLOG(stderr, "moving ..\n");
+        assert(len < sz);
+        q[tail] = std::move(val);
+        tail++;
+        len++;
+        return *this;
+    }
+    /**
+     * return reference to first element in queue
+     */
+    T& front() {
+        assert(len != 0);
+        return q[head];
+    }
+    /**
+     * return reference to last element in queue
+     */
+    T& back() {
+        assert(len != sz); // XXX: uhm, what? can this happen?
+        return q[tail];
+    }
+    /**
+     * remove front of queue and return it
+     *
+     * does not check that there's something in the queue
+     */
+    T pop() {
+        assert(len != 0);
+        auto ix = head;
+        head++;
+        len--;
+        return std::move(q[ix]);
+    }
+    /**
+     * return number of elements in queue
+     */
+    size_t size() {
+        return len;
+    }
+    /**
+     * return true if queue is empty
+     */
+    bool empty() {
+        return len == 0;
+    }
+private:
+    uint8_t space[sz * sizeof(T)]{};
+    struct QueueAccess {
+        T *ptr;
+        T& operator[](size_t i) {
+            assert(i < sz);
+            return ptr[i];
+        }
+    } q {(T*)space}; // Typed pointer into empty uint8_t[] space
     size_t len{};
     struct ArrayIndex {
         size_t val;
@@ -21,29 +90,7 @@ struct Queue {
             return ret;
         }
     } head{}, tail{};
-
-    bool empty() {
-        return len == 0;
-    }
-    void push(const T &val) {
-        assert(len < sz);
-        /* q[tail++] = T{val}; */
-        tail++;
-        len++;
-    }
-    T front() {
-        assert(len != 0);
-        return std::move(q[head]);
-    }
-    T& back() {
-        assert(len != sz);
-        return q[tail];
-    }
-    void pop() {
-        assert(len != 0);
-        /* if (len != 0) { */
-            head++;
-            len--;
-        /* } */
-    }
 };
+#ifdef UNDEF
+#undef DEBUGLOG
+#endif
