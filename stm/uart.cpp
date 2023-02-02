8ec5c5108811392875d6cbe0f03bc84ee832d35a
Jonathan Halmen <jonathan@halmen.org>
2023-02-02 10:38:42 +0100

    lots of uart changes

--- stm/uart.cpp
+++ stm/uart.cpp
@@ -3,44 +3,167 @@
  * Copyright (c) 2022 IACE
  */
 #include "uart.h"
-using UART_TX = Buffer<uint8_t>;
+struct SR {
+    uint32_t PE:1;
+    uint32_t FE:1;
+    uint32_t NF:1;
+    uint32_t ORE:1;
+    uint32_t IDLE:1;
+    uint32_t RXNE:1;
+    uint32_t TC:1;
+    uint32_t TXE:1;
+    uint32_t LBD:1;
+    uint32_t CTS:1;
+    uint32_t _:22;
+};
+struct DR {
+    uint32_t DR:9;
+    uint32_t _:23;
+};
+struct BRR {
+    uint32_t DIV_Fraction:4;
+    uint32_t DIV_Mantissa:12;
+    uint32_t _:16;
+};
+struct CR1 {
+    uint32_t SBK:1;
+    uint32_t RWU:1;
+    uint32_t RE:1;
+    uint32_t TE:1;
+    uint32_t IDLEIE:1;
+    uint32_t RXNEIE:1;
+    uint32_t TCIE:1;
+    uint32_t TXEIE:1;
+    uint32_t PEIE:1;
+    uint32_t PS:1;
+    uint32_t PCE:1;
+    uint32_t WAKE:1;
+    uint32_t M:1;
+    uint32_t UE:1;
+    uint32_t _:1;
+    uint32_t OVER8:1;
+    uint32_t _1:16;
+};
+struct CR2 {
+    uint32_t ADD:4;
+    uint32_t _:1;
+    uint32_t LBDL:1;
+    uint32_t LBDIE:1;
+    uint32_t _1:1;
+    uint32_t LBCL:1;
+    uint32_t CPHA:1;
+    uint32_t CPOL:1;
+    uint32_t CLKEN:1;
+    uint32_t STOP:2;
+    uint32_t LINEN:1;
+    uint32_t _2:17;
+};
+struct CR3 {
+    uint32_t EIE:1;
+    uint32_t IREN:1;
+    uint32_t IRLP:1;
+    uint32_t HDSEL:1;
+    uint32_t NACK:1;
+    uint32_t SCEN:1;
+    uint32_t DMAR:1;
+    uint32_t DMAT:1;
+    uint32_t RTSE:1;
+    uint32_t CTSE:1;
+    uint32_t CTSIE:1;
+    uint32_t ONEBIT:1;
+    uint32_t _:20;
+};
+struct GTPR {
+    uint32_t PSC:8;
+    uint32_t GT:8;
+    uint32_t _:16;
+};
+struct UART_INST {
+    struct SR SR;
+    struct DR DR;
+    struct BRR BRR;
+    struct CR1 CR1;
+    struct CR2 CR2;
+    struct CR3 CR3;
+    struct GTPR GTPR;
+} inst;
 
-/* HAL glue */
+/* General functions hidden from Interface */
+void startTransmit(HardwareUART *uart) {
+    if (uart->tx.q.empty()) return;
+    uart->tx.active = true;
+    auto &b = uart->tx.q.front();
+    auto ret = HAL_OK;
+    if ((ret = HAL_UART_Transmit_IT(&uart->handle, b.buf, b.len))) {
+        if (ret != HAL_OK) {
+            asm("bkpt");
+        }
+    }
+    uart->tx.timeout = Timeout{uwTick + 140};
+}
+void startReceive(HardwareUART *uart) {
+    auto ret = HAL_OK;
+    if ((ret = HAL_UARTEx_ReceiveToIdle_IT(&uart->handle, uart->rx.buf, uart->rx.size))) {
+        if (ret != HAL_OK) {
+            asm("bkpt");
+        }
+    }
+}
+
+void _errcallback(UART_HandleTypeDef *handle) {
+    auto uart = HardwareUART::reg.from(handle);
+    HAL_UART_Abort(handle);
+    startReceive(uart);
+    startTransmit(uart);
+}
+
+void poll(HardwareUART *uart) {
+    if (uart->tx.active && uart->tx.timeout(uwTick)) return _errcallback(&uart->handle);
+    if (!uart->tx.active) return startTransmit(uart);
+}
+
 void _rxevent(UART_HandleTypeDef *handle, uint16_t nbs) {
-    HardwareUART::reg.from(handle)->rxevent(nbs);
+    auto uart = HardwareUART::reg.from(handle);
+    uart->rx.len = nbs;
+    uart->listener.push(std::move(uart->rx));
+    uart->rx = Buffer<uint8_t>{512};
+    startReceive(uart);
 }
 void _rxcallback(UART_HandleTypeDef *handle) {
-    HardwareUART::reg.from(handle)->rxcallback();
+    auto uart = HardwareUART::reg.from(handle);
+    _rxevent(handle, uart->rx.size);
 }
 void _txcallback(UART_HandleTypeDef *handle) {
-    HardwareUART::reg.from(handle)->txcallback();
+    auto uart = HardwareUART::reg.from(handle);
+    auto& tx = uart->tx;
+    tx.q.pop();
+    tx.active = false;
+    tx.timeout = Timeout{};
+    poll(uart);
 }
-void _errcallback(UART_HandleTypeDef *handle) {
-    auto led = DIO(GPIO_PIN_13, GPIOC);
-    led.set(true);
+void init(HardwareUART *uart, UART_HandleTypeDef *handle) {
+    HardwareUART::reg.reg(uart, handle);
+    while (HAL_UART_Init(handle) != HAL_OK);
+    HAL_UART_RegisterRxEventCallback(handle, _rxevent);
+    HAL_UART_RegisterCallback(handle, HAL_UART_RX_COMPLETE_CB_ID, _rxcallback);
+    HAL_UART_RegisterCallback(handle, HAL_UART_TX_COMPLETE_CB_ID, _txcallback);
+    HAL_UART_RegisterCallback(handle, HAL_UART_ERROR_CB_ID, _errcallback);
+    startReceive(uart);
 }
 
-/**
- * Initialize the peripheral
- *
- * make sure to also initialize the corresponding rx/tx AFIO pins
- *
- * @param dUsart definition of used UART
- * @param iBaudRate baud rate
- */
-HardwareUART::HardwareUART(
-        USART_TypeDef *dUsart,
-        uint32_t iBaudRate,
-        ByteHandler *l
-        ) :
-    RequestQueue<UART_TX>(30, 0),
-    listener(l)
-{
-    reg.reg(this, &handle);
+// class functions
+HardwareUART::HardwareUART(const Manual &conf) : listener(conf.handler) {
     handle = {
-        .Instance = dUsart,
+        .Instance = conf.uart,
+        .Init = conf.init,
+    };
+    init(this, &handle);
+}
+HardwareUART::HardwareUART(const Default &conf) : listener(conf.handler) {
+    handle = {
+        .Instance = conf.uart,
         .Init = {
-            .BaudRate = iBaudRate,
+            .BaudRate = conf.baudrate,
             .WordLength = UART_WORDLENGTH_8B,
             .StopBits = UART_STOPBITS_1,
             .Parity = UART_PARITY_NONE,
@@ -49,68 +172,20 @@ HardwareUART::HardwareUART(
             .OverSampling = UART_OVERSAMPLING_16,
         },
     };
-    while (HAL_UART_Init(&handle) != HAL_OK);
-    HAL_UART_RegisterRxEventCallback(&handle, _rxevent);
-    HAL_UART_RegisterCallback(&handle, HAL_UART_RX_COMPLETE_CB_ID, _rxcallback);
-    HAL_UART_RegisterCallback(&handle, HAL_UART_TX_COMPLETE_CB_ID, _txcallback);
-    HAL_UART_RegisterCallback(&handle, HAL_UART_ERROR_CB_ID, _errcallback);
-    HAL_UARTEx_ReceiveToIdle_IT(&handle, listenbuf, sizeof listenbuf);
+    init(this, &handle);
 }
-
-void HardwareUART::rxevent(uint16_t nbs) {
-    uint16_t workleft = handle.RxXferSize - nbs;
-    uint8_t *p = handle.pRxBuffPtr;
-    if (workleft < 64) {
-        workleft = sizeof listenbuf;
-        p = listenbuf;
-    }
-    uint8_t *tmp = handle.pRxBuffPtr - nbs;
-    HAL_UARTEx_ReceiveToIdle_IT(&handle, p, workleft);
-    for (uint16_t i = 0; i < nbs; ++i) {
-        listener->recv(tmp[i]);
-    }
-}
-void HardwareUART::rxcallback() {
-    /* auto r = RequestQueue<UART_RX>::rqCurrent(); */
-    /* if (r->cb) r->cb(); */
-    /* HAL_UART_AbortReceive_IT(&handle); */
-    /* RequestQueue<UART_RX>::rqEnd(); */
-}
-void HardwareUART::txcallback() {
-    rqEnd();
-    /* auto r = RequestQueue<UART_TX>::rqCurrent(); */
-    /* if (r->cb) r->cb(); */
-    /* RequestQueue<UART_TX>::rqEnd(); */
-}
-
-/* short HardwareUART::request(UART_TX *r) { */
-/*     return RequestQueue<UART_TX>::request(r); */
-/* } */
-/* short HardwareUART::request(UART_RX *r) { */
-/*     return RequestQueue<UART_RX>::request(r); */
-/* } */
 
 void HardwareUART::irqHandler() {
     HAL_UART_IRQHandler(&handle);
+    /** workaround for HAL BUG: they still change state _AFTER_ calling */
+    /** user callbacks (where we had already set it to what we needed) */
+    /** luckily we only ever use this one type of reception */
+    handle.ReceptionType = HAL_UART_RECEPTION_TOIDLE;
 }
-
-void HardwareUART::rqBegin(UART_TX *r) {
-    auto ret = HAL_UART_Transmit_IT(&handle, r->payload, r->len);
-    if (ret != HAL_OK) {
-        asm("bkpt");
-    }
-
+void HardwareUART::push(const Buffer<uint8_t> &b) {
+    push(std::move(Buffer<uint8_t>{b}));
 }
-/* void HardwareUART::rqBegin(UART_RX *r) { */
-/*     auto ret = HAL_UART_Receive_IT(&handle, r->buf, r->len); */
-/*     if (ret != HAL_OK) { */
-/*         asm("bkpt"); */
-/*     } */
-/* } */
-/* we don't use these */
-void HardwareUART::rqTimeout(UART_TX *r) { /* not implemented */ }
-unsigned long HardwareUART::getTime() { return 0; }
-/* void HardwareUART::listen(void (*func)(uint8_t c)) { */
-/*     assert(listener == nullptr); */
-/*     listener = func; */
-/* } */
+void HardwareUART::push(Buffer<uint8_t> &&b) {
+    tx.q.push(std::move(b));
+    poll(this);
+}
--- stm/uart.h
+++ stm/uart.h
@@ -9,73 +9,52 @@
 #include "stm/registry.h"
 #include "utils/RequestQueue.h"
 #include "utils/Buffer.h"
+#include "x/Timeout.h"
 #include "hw/uart.h"
 
-class HardwareUART : public RequestQueue<Buffer<uint8_t>>{
-public:
-    using UART_TX = Buffer<uint8_t>;
-    inline static Registry<HardwareUART, UART_HandleTypeDef, 8> reg;
-    uint8_t listenbuf[512];
-    ByteHandler *listener;
+struct HardwareUART : public Push<Buffer<uint8_t>> {
+    // Init Structs
+    struct Default {
+        USART_TypeDef *uart;
+        AFIO rx, tx;
+        uint32_t baudrate;
+        Push<Buffer<uint8_t>> &handler;
+    };
+    struct Manual {
+        USART_TypeDef *uart;
+        AFIO rx, tx;
+        UART_InitTypeDef init;
+        Push<Buffer<uint8_t>> &handler;
+    };
     /**
-     * Initialize the peripheral
-     *
-     * make sure to also initialize the corresponding rx/tx AFIO pins
-     *
-     * @param dUsart definition of used UART
-     * @param iBaudRate baud rate
+     * constructor with default config
      */
-    HardwareUART(USART_TypeDef *dUsart, uint32_t iBaudRate, ByteHandler *l);
-    void rxevent(uint16_t nbs);
-    void rxcallback();
-    void txcallback();
+    HardwareUART(const Default &conf);
+    /**
+     * constructor with manually specifiable settings
+     */
+    HardwareUART(const Manual &conf);
+    /**
+     * push bytebuffer into sending queue
+     */
+    void push(Buffer<uint8_t> &&tx) override;
+    void push(const Buffer<uint8_t> &tx) override;
     /** interrupt handler
      *
      * call this directly in interrupt
      */
     void irqHandler();
+    inline static Registry<HardwareUART, UART_HandleTypeDef, 8> reg;
+    // rx
+    Buffer<uint8_t> rx{512};
+    Push<Buffer<uint8_t>> &listener;
+    // tx state
+    struct {
+        Queue<Buffer<uint8_t>, 30> q;
+        Timeout timeout;
+        bool active;
+    } tx{};
+
     /** HAL handle for use with unwrapped UART capabilities */
     UART_HandleTypeDef handle{};
-    /* queue defs */
-    /* short request(UART_TX *r) override; */
-    void rqBegin(UART_TX *r) override;
-    void rqTimeout(UART_TX *r) override;
-    unsigned long getTime() override;
-};
-
-struct UART: RequestQueue<UARTTxRequest> {
-    struct Basic {
-        USART_TypeDef *uart;
-        AFIO rx, tx;
-        UART_InitTypeDef init;
-        ByteHandler *handler;
-        size_t qlen;
-        size_t timeout;
-    };
-    UART(Basic b)
-            : RequestQueue<UARTTxRequest>(b.qlen, b.timeout)
-            , handler(b.handler)
-    {
-        handle.Instance = b.uart;
-        handle.Init = b.init;
-        while(HAL_UART_Init(&handle) != HAL_OK);
-        /* HAL_UART_Receive */
-    }
-    UART_HandleTypeDef handle{};
-    ByteHandler *handler{};
-
-    void txcallback() {
-        rqEnd();
-    }
-    void rqBegin(UARTTxRequest *r) override {
-        HAL_UART_Transmit_IT(&handle, r->payload, r->len);
-    }
-    void rqTimeout(UARTTxRequest *r) override {
-    }
-    unsigned long getTime() override {
-        return 0;
-    }
-    void irqHandler() {
-        HAL_UART_IRQHandler(&handle);
-    }
 };
