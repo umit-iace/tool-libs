77a1bb21e5e83fd30a8acc5a53d599e3256d2a0b
Jonathan Halmen <jonathan@halmen.org>
2023-02-09 18:11:19 +0100

    x/tfr: cleanup

--- x/TimedFuncRegistry.h
+++ x/TimedFuncRegistry.h
@@ -2,27 +2,19 @@
 #include "utils/Buffer.h"
 #include "utils/Queue.h"
 #include "x/Schedule.h"
-#define min(a,b) ((a)<(b)?(a):(b))
-struct TimedFuncRegistry//: public Scheduler {
-{
+
+struct TimedFuncRegistry {
     using Func = void (*)(uint32_t, uint32_t);
+    inline uint32_t min(uint32_t a, uint32_t b) { return a < b ? a : b; }
     struct Container {
         Func func;
         uint32_t dt_ms, next;
     };
-    /* struct Callable { */
-    /*     Func func; */
-    /*     uint32_t sched_time; */
-    /*     uint32_t dt_ms; */
-    /*     void call() { */
-    /*         func(sched_time, dt_ms); */
-    /*     } */
-    /* }; */
 
-    Buffer<Container> list{20};
-    uint32_t next;
-    /* Queue<Callable, 20> q; */
+    Buffer<Container> list{20}; //< list of registered functions to be scheduled
+    uint32_t next; //< next point in time when this registry shall be called
 
+    /** register a function to be called every @dt_ms */
     void reg(Func func, uint32_t dt_ms) {
         list.append({.func = func, .dt_ms = dt_ms});
         next = 0;
@@ -34,23 +26,16 @@ struct TimedFuncRegistry//: public Scheduler {
             if (c.next == 0 || time+1 >= c.next) {
                 c.next += c.dt_ms;
                 next = min(next, c.next);
-            /* if (c.last == 0 || time+1 >= c.last + c.dt_ms) { */
-            /*     c.last = time+1; */
                 // schedule the call of the registered function
                 s.push(new TimeSchedulableFunc(c.func, {time, c.dt_ms}));
             }
         }
     }
+    /** reset calling times of all registered functions */
     void reset() {
         next = 0;
         for (auto &c: list) {
             c.next = 0;
         }
     }
-    /* void run() override { */
-    /*     while (!q.empty()) { */
-    /*         q.pop().call(); */
-    /*     } */
-    /* } */
 };
-#undef min
