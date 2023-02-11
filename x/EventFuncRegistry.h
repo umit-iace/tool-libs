91b16f35329593da7f2e9e3953fed2f86eb02166
Jonathan Halmen <jonathan@halmen.org>
2023-02-11 17:24:22 +0100

    x/*: start cleaning up scheduling registries

--- x/EventFuncRegistry.h
+++ x/EventFuncRegistry.h
@@ -2,11 +2,39 @@
 #include "utils/Buffer.h"
 #include "x/Schedule.h"
 
+struct EventSchedulable: public SchedulableBase {
+    uint32_t time{};
+    bool schedule(uint32_t t) override {
+        time = t;
+        return true;
+    }
+};
+
+struct EventSchedulableFunc: public EventSchedulable {
+    using Func = void (*)(uint32_t);
+    Func func{};
+    EventSchedulableFunc(Func f): func(f) { }
+    void call() override {
+        return func(time);
+    }
+};
+template<typename T>
+struct EventSchedulableMethod: public EventSchedulable {
+    using Base = T*;
+    using Method = void (T::*)(uint32_t);
+    Base base;
+    Method method;
+    EventSchedulableMethod(Base b, Method m): base(b), method(m) { }
+    void call() override {
+        return (base->*method)(time);
+    }
+};
+
 struct EventFuncRegistry {
     using Func = void (*)(uint32_t);
     template<typename T>
     using Method = void (T::*)(uint32_t);
-    Buffer<SchedulableBase *> list{20};
+    Buffer<EventSchedulable *> list{20};
     ~EventFuncRegistry() {
         for (auto &entry : list) {
             delete entry;
@@ -14,17 +42,15 @@ struct EventFuncRegistry {
     }
     /** register Function to be scheduled for calling on Event */
     void onEvent(Func func) {
-        list.append(new EventSchedulableFunc{func, {0}});
+        list.append(new EventSchedulableFunc{func});
     }
     template<typename T>
     void onEvent(T& base, Method<T> method) {
-        list.append(new EventSchedulableMethod<T>(&base, method, {0}));
+        list.append(new EventSchedulableMethod<T>{&base, method});
     }
     void schedule(Scheduler &s, uint32_t time) {
         for (auto &f : list) {
-            f->schedule(s.q, time);
+            if (f->schedule(time)) s.push(f);
         }
-            /* s.push(new EventSchedulableFunc(f, {time})); */
-        /* } */
     }
 };
--- x/TimedFuncRegistry.h
+++ x/TimedFuncRegistry.h
@@ -3,6 +3,38 @@
 #include "utils/Queue.h"
 #include "x/Schedule.h"
 
+struct TimeSchedulable: public SchedulableBase {
+    uint32_t next{}, time{}, dt{};
+    TimeSchedulable(uint32_t dt) : dt{dt} { }
+    bool schedule(uint32_t now) override {
+        if (now < next) return false;
+        next += dt;
+        time = now;
+        return true;
+    }
+    void reset() { next = 0; }
+};
+
+struct TSFunc: public TimeSchedulable {
+    using Func = void (*)(uint32_t, uint32_t);
+    Func func;
+    TSFunc(Func f, uint32_t dt): TimeSchedulable(dt), func(f) { }
+    void call() override { return func(time, dt); }
+};
+
+template<typename T>
+struct TSMethod : public TimeSchedulable {
+    using Base = T*;
+    using Method = void (T::*)(uint32_t, uint32_t);
+    Base base;
+    Method method;
+    TSMethod(Base b, Method m, uint32_t dt)
+        : TimeSchedulable(dt), base(b), method(m) { }
+    void call() override {
+        return (base->*method)(time, dt);
+    }
+};
+
 struct TimedFuncRegistry {
     using Func = void (*)(uint32_t, uint32_t);
     template <typename T>
@@ -12,20 +44,20 @@ struct TimedFuncRegistry {
             delete entry;
         }
     }
-    Buffer<SchedulableBase *> list{20}; //< list of registered functions to be scheduled
+    Buffer<TimeSchedulable *> list{20}; //< list of registered functions to be scheduled
 
     /** register a function to be called every @dt_ms */
     void every(uint32_t dt_ms, Func func) {
-        list.append(new TimeSchedulableFunc{func, {.dt_ms = dt_ms}});
+        list.append(new TSFunc{func, dt_ms});
     }
     template<typename T>
     void every(uint32_t dt_ms, T& base, Method<T> method) {
-        list.append(new TimeSchedulableMethod<T>(&base, method, {.dt_ms=dt_ms}));
+        list.append(new TSMethod<T>{&base, method, dt_ms});
     }
 
     void schedule(Scheduler &s, uint32_t time) {
         for (auto &c : list) {
-            c->schedule(s.q, time);
+            if (c->schedule(time)) s.push(c);
         }
     }
     /** reset calling times of all registered functions */
