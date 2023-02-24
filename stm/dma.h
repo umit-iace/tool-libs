cd8479fb2b30ed053ee4bbaaa53de6fb01965a73
Jonathan Halmen <jonathan@halmen.org>
2023-02-24 16:52:27 +0100

    stm/dma: docs

--- stm/dma.h
+++ stm/dma.h
@@ -2,27 +2,27 @@
  *
  * Copyright (c) 2022 IACE
  */
-#ifndef STM_DMA_H
-#define STM_DMA_H
+#pragma once
 #include "hal.h"
 
-/**
- * initializing wrapper for circular inbound DMA
- */
+/** initializing wrapper for DMA peripheral */
 class HardwareDMA {
 public:
+    /** DMA configuration structure */
     struct Conf {
         DMA_Stream_TypeDef *stream;
         DMA_InitTypeDef init;
     };
+    /** initialize DMA peripheral */
     HardwareDMA(Conf conf) {
         handle.Instance = conf.stream;
         handle.Init = conf.init;
         while (HAL_DMA_Init(&handle) != HAL_OK);
     }
+    /** call directly in interrupt */
     void irqHandler() {
         HAL_DMA_IRQHandler(&handle);
     }
+    /** HAL handle, for use with unwrapped DMA capabilities */
     DMA_HandleTypeDef handle{};
 };
-#endif //STM_DMA_H
