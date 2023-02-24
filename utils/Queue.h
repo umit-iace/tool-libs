a5c4509c3b78ebe7b569172fc78fed0f39962e49
Jonathan Halmen <jonathan@halmen.org>
2023-02-24 20:22:31 +0100

    utils/Queue: remove size from template
    make Queue depend on Buffer for storage, this makes queues be allocated,
    but it also means that queue sizes don't have to be hardcoded into every
    Queue user

--- utils/Queue.h
+++ utils/Queue.h
@@ -8,50 +8,51 @@
 #include <cstdint>
 
 #include "Interfaces.h"
+#include "Buffer.h"
 
-/**
- * simple Array backed queue implementation
- */
-template <typename T, int sz>
+/** simple Buffer backed queue implementation */
+template <typename T>
 class Queue : public Sink<T>, public Source<T> {
-    uint8_t space[sz * sizeof(T)]{};
-    struct QueueAccess {
-        T *ptr;
-        T& operator[](size_t i) {
-            assert(i < sz);
-            return ptr[i];
-        }
-    } q {(T*)space}; // Typed pointer into empty uint8_t[] space
-    size_t len{};
-    struct ArrayIndex {
+    union {
+        Buffer<uint8_t> space;  // space
+        Buffer<T> q;            // access
+    };
+    struct WrappingIndex {
+        WrappingIndex(size_t sz) : sz(sz) { }
         size_t val{};
+        const size_t sz{};
         operator size_t() {
             return val;
         }
-        size_t operator++() {
+        size_t operator++(int) {
+            auto tmp = val;
             val = (val + 1) % sz;
-            return val;
+            return tmp;
         }
     } head, tail;
 public:
+    ~Queue() { space.~Buffer(); }
+    /** create Queue with constant size */
+    Queue(size_t size=30) : space{size * sizeof(T)}, head{size}, tail{size} {
+        // now that we allocated enough space,
+        // we can use the size & len for our own purposes
+        q.size = size;
+        q.len = 0;
+    }
     using Sink<T>::push;
-    /** move element into queue */
+    /** move element into queue
+     *
+     * consider guarding with `if(! full()) ...`
+     **/
     void push(T &&val) override {
-        assert(len < sz);
-        if (len == 0) tail = {0};
-        else ++tail;
-        q[tail] = std::move(val);
-        ++len;
+        assert(q.len < q.size);
+        q[tail++] = std::move(val);
+        q.len++;
     }
     /** return reference to first element in queue */
     T& front() {
-        assert(len != 0);
+        assert(q.len != 0);
         return q[head];
-    }
-    /** return reference to last element in queue */
-    T& back() {
-        assert(len <= sz);
-        return q[tail];
     }
     /**
      * remove front of queue and return it
@@ -60,28 +61,20 @@ public:
      * guard with `if (size()) ...` or `if (! empty()) ...`
      */
     T pop() override {
-        assert(len != 0);
-        auto ix = head;
-        ++head;
-        --len;
-        // let queue always start at the beginning
-        // if we ever get it empty
-        if (len == 0) {
-            head = {0};
-            tail = {0};
-        }
-        return std::move(q[ix]);
+        assert(q.len != 0);
+        q.len++;
+        return std::move(q[head++]);
     }
     /** return number of elements in queue */
     size_t size() {
-        return len;
+        return q.len;
     }
     /** check if queue is empty */
     bool empty() override {
-        return len == 0;
+        return q.len == 0;
     }
     /** return true if queue is full */
     bool full() {
-        return len == sz;
+        return q.len == q.size;
     }
 };
