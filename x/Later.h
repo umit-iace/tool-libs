b3b85fffd92fd17ca7ab6146840751b70eb55cb7
Jonathan Halmen <jonathan@halmen.org>
2023-03-12 14:32:32 +0100

    x/Later: add 'new' operators
    this allows us to actually switch out a Later object during runtime,
    e.g. when enabling an observer, or a feed forward

--- x/Later.h
+++ x/Later.h
@@ -1,6 +1,7 @@
 #pragma once
+#include <utility>
 
-/** Deferred Additions & Subtractions until access
+/** Defer Additions & Subtractions until access
  *
  * this is the poor man's closure for lambda functions
  *
@@ -8,9 +9,9 @@
  * ```
  * int a{8}, b{3}, c{4};
  * auto op = (Later<int>) a + b - c;
- * op.get() /// -> 7
+ * op.get() // -> 7
  * a = 4;
- * op.get() /// -> 3
+ * op.get() // -> 3
  * ```
  */
 template<typename T>
@@ -18,7 +19,7 @@ struct Later {
     T& where{};
     Later *nested{};
     enum Op {NOP, ADD, SUB} op{};
-    ~Later() { delete nested; nested = nullptr;}
+    ~Later() { delete nested; nested = nullptr; }
     Later(T& val): where(val) {} // unable to store temporaries. cast the first operand to this type first
     Later(T& v, Later *n, Op op): where(v), nested(n), op(op) {}
     // copying impossible. who'd own nested Laters?
@@ -48,12 +49,20 @@ struct Later {
             SUB,
         };
     }
-    /** explicit accessor */
+    /** get value */
     T get() const {
         switch (op) {
         case ADD: return nested->get() + where;
         case SUB: return nested->get() - where;
         default: return where;
         }
+    }
+    // placement new for copying into place
+    void *operator new(size_t sz, Later *where) {
+        return where;
+    }
+    // normal new
+    void *operator new(size_t sz) {
+        return ::operator new(sz);
     }
 };
