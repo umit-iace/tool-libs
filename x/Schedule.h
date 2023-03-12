1ca19ab7206e1ff8fa6c28aee58157ff3430a091
Jonathan Halmen <jonathan@halmen.org>
2023-03-12 16:33:43 +0100

    x/schedule: more documentation

--- x/Schedule.h
+++ x/Schedule.h
@@ -4,12 +4,25 @@ namespace Schedule {
 /** represents an object that can be scheduled for later calling
  *
  * is the poor man's closure
+ * this can be implemented to enable different behaviours. See the
+ * Recurring & Evented namespaces
  */
 struct Schedulable {
     virtual ~Schedulable(){}
-    virtual bool schedule(uint32_t time){return true;}
+    /** return true if object should be scheduled at this time */
+    virtual bool schedule(uint32_t now) { return true; }
+    /** this method is called to run the Schedulable */
     virtual void call() =0;
 };
+/** a registry of Schedulable which can be given to the Scheduler for
+ * actual scheduling of calls
+ *
+ * this is implemented / extended for the actual registration of the
+ * specific schedulables. see Evented and Recurring
+ *
+ * useful to have a bunch of these for the different situations
+ * where scheduling is necessary. see Experiment for inspiration
+ */
 struct Registry {
     Buffer<Schedulable *> list{20};
     ~Registry() {
@@ -25,12 +38,13 @@ struct Scheduler {
     // this is pointing to the actual callables in the registries
     // we do not own them
     Queue<Schedule::Schedulable*> q;
-
+    /** schedule all registered schedulables of registry if necessary */
     void schedule(uint32_t time, Schedule::Registry &reg) {
         for (auto &c: reg.list) {
             if (c->schedule(time)) q.push(c);
         }
     }
+    /** run scheduled calls */
     void run() {
         while (!q.empty()) {
             auto s = q.pop();
