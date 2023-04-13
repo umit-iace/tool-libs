6615d1d14ec9097e05ee9daa5f8e127aea5e51a1
Jonathan Halmen <jonathan@halmen.org>
2023-04-13 16:53:25 +0200

    sensors: start using new i2c structure

--- sensors/AS5048b.h
+++ sensors/AS5048b.h
@@ -2,54 +2,42 @@
  *
  * Copyright (c) 2023 IACE
  */
-#ifndef AS5048B_H
-#define AS5048B_H
+#pragma once
 
+#include <cmath>
 #include <cstdint>
-#include "stm/hal.h"
 #include "stm/i2c.h"
 
-#define M_PI 3.14159265358979323846
-
-class AS5048B : I2CDevice {
+class AS5048B : I2C::Device {
 public:
 
     /**
      * initialize the sensor
      */
-    AS5048B(RequestQueue<I2CRequest> *bus,
-            bool A0, bool A1) : I2CDevice((0x40 << 0) | ((A0 ? 1 : 0) << 0) | ((A1 ? 1 : 0) << 1)),
-                                bInit(false), bus(bus) {
-    }
+    AS5048B(Sink<I2C::Request> *bus, bool A0, bool A1)
+        : I2C::Device{*bus, (uint8_t)(0x40 | (A1 << 1) | A0)}
+        , bInit(false)
+    { }
 
     /**
      * start async read of angle
      */
     void measure() {
-        bus->request(new I2CRequest(
-                this,
-                0xfe,
-                (uint8_t *) this->raw,
-                2,
-                I2CRequest::I2C_MEM_READ,
-                (void *) true)
-        );
+        bus.push({
+            .dev = this,
+            .data = Buffer<uint8_t>{2},
+            .opts = {
+                .read = true,
+                .mem = true,
+                },
+            .mem = 0xfe,
+            });
     }
 
-    void writeReg(uint8_t reg, uint8_t val) {
-        bus->request(new I2CRequest(
-                this,
-                reg,
-                &val,
-                1,
-                I2CRequest::I2C_MEM_WRITE,
-                nullptr)
-        );
-    }
-
-    void callback(void *cbData) override {
-        uint16_t iRawAngle = ((uint16_t) this->raw[0] << 6);
-        iRawAngle += (this->raw[1] & 0x3F);
+    void callback(const I2C::Request &rq) override {
+        if (rq.mem != 0xfe) return;
+        uint16_t iRawAngle = ((uint16_t) rq.data.buf[0] << 6);
+        iRawAngle += (rq.data.buf[1] & 0x3F);
 
         if (!bInit) {
             this->dLastAngle = ((double) iRawAngle);
@@ -71,7 +59,7 @@ public:
     }
 
     /**
-     * Returns the angle in radiant
+     * Returns the angle in radians
      */
     double get() {
         return this->dAngle / 16384 * 2 * M_PI;
@@ -79,14 +67,9 @@ public:
 
 private:
     ///\cond false
-    uint8_t raw[2] = {};
     bool bInit = false;
 
     double dAngle = 0;              // difference angle for rotational movement
     double dLastAngle = 0;          // difference angle for rotational movement
-    RequestQueue<I2CRequest> *bus;
-
     ///\endcond
 };
-
-#endif //AS5048B_H
