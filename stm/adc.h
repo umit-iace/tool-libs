e053cf862ba3f50bc4b053a8f23b6e947bb7551a
Jens Wurm <jens.wurm@umit.at>
2023-05-04 19:22:30 +0200

    First try of adc impl

--- stm/adc.h
+++ stm/adc.h
@@ -3,84 +3,56 @@
  * Copyright (c) 2023 IACE
  */
 #pragma once
+
 #include <utils/queue.h>
 
 #include "gpio.h"
 #include "registry.h"
-namespace AdC {
 
-struct Channel {
+namespace Adc {
 
-    Channel() {}
-    /**
-     * callback. is called as soon as transmission completed successfully
-     */
-    virtual void callback()=0;
-};
+    struct HW {
+        inline static Registry<HW, ADC_HandleTypeDef, 4> reg{};
 
-struct HW {
-    inline static Registry<HW, ADC_HandleTypeDef, 4> regADC{};
-    inline static Registry<HW, DMA_HandleTypeDef, 4> regDMA{};
+        struct ChannelConf {
+            uint32_t channel;
+            uint8_t rank;
+            uint32_t sampleTime;
+            DIO pin;
+        };
 
-    struct Conf {
-        ADC_TypeDef *adc; ///< ADC peripheral
-        DMA_TypeDef *dma; ///< DMA peripheral
-    };
-    HW(const Conf &conf) {
-        iBuffer = new uint32_t[iBufferCount];
-        for(uint8_t i = 0; i < iBufferCount; ++i) {
-            iBuffer[i] = 0;
+        struct Conf {
+            ADC_TypeDef *adc;
+            DMA_Stream_TypeDef *dmaStream;
+            uint32_t dmaChannel;
+            IRQn_Type adcDmaInterrupt;
+            uint32_t adcDmaPrePrio;
+            uint32_t adcDmaSubPrio;
+            uint8_t adcChannelCount;
+            ChannelConf channels[2];
+        };
+
+        HW(const Conf &conf);
+
+        void configADC(ADC_TypeDef *dADC);
+        void configDMA(DMA_Stream_TypeDef *dDMA, uint32_t iDMAChannel,
+                       IRQn_Type iAdcDmaInterrupt, uint32_t iAdcDmaPrePrio,
+                       uint32_t iAdcDmaSubPrio);
+        void configChannel(const ChannelConf &conf );
+        void irqHandler(void);
+
+        void measure() {
+            HAL_ADC_Start(&hAdc);
         }
+
+        double getBuffer(uint8_t idx) {
+            return this->iBuffer[idx];
+        }
+
+    private:
+        uint8_t adcChannelCount = 0;
+        ADC_HandleTypeDef hAdc{};
+        DMA_HandleTypeDef hDma{};
+        uint32_t *iBuffer;
     };
-
-    void configADC(ADC_TypeDef *dADC) {
-        this->hAdc.Instance = dADC;
-        this->hAdc.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
-        this->hAdc.Init.Resolution = ADC_RESOLUTION_12B;
-        this->hAdc.Init.ScanConvMode = ENABLE;
-        this->hAdc.Init.ContinuousConvMode = DISABLE;
-        this->hAdc.Init.DiscontinuousConvMode = DISABLE;
-        this->hAdc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-        this->hAdc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-        this->hAdc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-        this->hAdc.Init.NbrOfConversion = this->iBufferCount;
-        this->hAdc.Init.DMAContinuousRequests = ENABLE;
-        this->hAdc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-        while (HAL_ADC_Init(&this->hAdc) != HAL_OK);
-    }
-
-    void configDMA(DMA_Stream_TypeDef *dDMA, uint32_t iDMAChannel,
-                   IRQn_Type iAdcDmaInterrupt, uint32_t iAdcDmaPrePrio, uint32_t iAdcDmaSubPrio) {
-        this->hDma.Instance = dDMA;
-        this->hDma.Init.Channel = iDMAChannel;
-        this->hDma.Init.Direction = DMA_PERIPH_TO_MEMORY;
-        this->hDma.Init.PeriphInc = DMA_PINC_DISABLE;
-        this->hDma.Init.MemInc = DMA_MINC_ENABLE;
-        this->hDma.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
-        this->hDma.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
-        this->hDma.Init.Mode = DMA_CIRCULAR;
-        this->hDma.Init.Priority = DMA_PRIORITY_MEDIUM;
-        this->hDma.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
-        while(HAL_DMA_Init(&this->hDma) != HAL_OK);
-
-        __HAL_LINKDMA(&this->hAdc, DMA_Handle, this->hDma);
-
-        HAL_ADC_RegisterCallback(&this->hAdc, HAL_ADC_CONVERSION_COMPLETE_CB_ID, conversionComplete);
-
-        HAL_NVIC_SetPriority(iAdcDmaInterrupt, iAdcDmaPrePrio, iAdcDmaSubPrio);
-        HAL_NVIC_EnableIRQ(iAdcDmaInterrupt);
-
-        /// start
-        while(HAL_ADC_Start_DMA(&this->hAdc, this->iBuffer, this->iBufferCount) != HAL_OK);
-    }
-
-    static void conversionComplete(ADC_HandleTypeDef *hAdc) {
-    }
-
-private:
-    uint8_t iBufferCount = 0;
-    ADC_HandleTypeDef hAdc{};
-    DMA_HandleTypeDef hDma{};
-    uint32_t* iBuffer;
-};
 }
--- stm/pwm.h
+++ stm/pwm.h
@@ -13,60 +13,58 @@
  */
 namespace PWM {
 
-class HW {
-public:
-    /**
-     * set pwm in percent
-     * @param perc [0..1]
-     */
-    void pwm(double perc) {
-        ticks(perc * period);
-    }
+    class HW {
+    public:
+        /**
+         * set pwm in percent
+         * @param perc [0..1]
+         */
+        void pwm(double perc) {
+            ticks(perc * period);
+        }
 
-    /**
-     * set pwm as value of timer ticks
-     * @param ticks
-     */
-     void ticks(uint32_t ticks) {
-         __HAL_TIM_SET_COMPARE(&hTim, chan, ticks);
-     }
+        /**
+         * set pwm as value of timer ticks
+         * @param ticks
+         */
+        void ticks(uint32_t ticks) {
+            __HAL_TIM_SET_COMPARE(&hTim, chan, ticks);
+        }
 
-     struct Config {
-         HardwareTimer tim;
-         uint32_t chan;
-         AFIO pin;
-     };
-    /**
-     * Initialize Timer Channel
-     *
-     * make sure to initialize the corresponding AFIO pin
-     *
-     * @param tim pointer to HardwareTimer
-     * @param chan timer channel number (TIM_CHANNEL_x)
-     */
-    HW(const Config &conf)
-        : hTim(conf.tim.handle)
-        , chan(conf.chan)
-        , period(hTim.Init.Period)
-    {
-        // pwm config of timer
-        while (HAL_TIM_PWM_Init(&hTim) != HAL_OK);
+        struct Config {
+            HardwareTimer tim;
+            uint32_t chan;
+            AFIO pin;
+        };
 
-        // channel config
-        TIM_OC_InitTypeDef sConfigOC = {};
-        sConfigOC.OCMode = TIM_OCMODE_PWM1;
-        sConfigOC.Pulse = 0;
-        sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-        sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-        while (HAL_TIM_PWM_ConfigChannel(&hTim, &sConfigOC, chan) != HAL_OK);
+        /**
+         * Initialize Timer Channel
+         *
+         * make sure to initialize the corresponding AFIO pin
+         *
+         * @param tim pointer to HardwareTimer
+         * @param chan timer channel number (TIM_CHANNEL_x)
+         */
+        HW(const Config &conf)
+                : hTim(conf.tim.handle), chan(conf.chan), period(hTim.Init.Period) {
+            // pwm config of timer
+            while (HAL_TIM_PWM_Init(&hTim) != HAL_OK);
 
-        __HAL_TIM_SET_COMPARE(&hTim, chan, 0);
-        while (HAL_TIM_PWM_Start(&hTim, chan) != HAL_OK);
-    }
+            // channel config
+            TIM_OC_InitTypeDef sConfigOC = {};
+            sConfigOC.OCMode = TIM_OCMODE_PWM1;
+            sConfigOC.Pulse = 0;
+            sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+            sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+            while (HAL_TIM_PWM_ConfigChannel(&hTim, &sConfigOC, chan) != HAL_OK);
 
-private:
-    TIM_HandleTypeDef hTim{};
-    uint32_t chan= 0;
-    uint32_t period = 0;
-};
+            __HAL_TIM_SET_COMPARE(&hTim, chan, 0);
+            while (HAL_TIM_PWM_Start(&hTim, chan) != HAL_OK);
+        }
+
+    private:
+        TIM_HandleTypeDef hTim{};
+        uint32_t chan = 0;
+        uint32_t period = 0;
+    };
 }
