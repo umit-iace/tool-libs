c415e22f2994c8b543d2b190b0722ce710a6b5ea
Jonathan Halmen <jonathan@halmen.org>
2023-05-23 14:03:15 +0200

    test: add new `later` test-case

--- tests/later.cpp
+++ tests/later.cpp
@@ -21,16 +21,28 @@ TEST_CASE("tool-libs: later: ") {
     b.d = 0.1;
     CHECK(d.get() == Approx(a+c-b));
 
-    SUBCASE("reassigning") {
-        d = (Later<double>)c+b;
-        CHECK(d.get() == Approx(c+b));
-        c.d = 3.33;
-        CHECK(d.get() == Approx(c+b));
-    }
     SUBCASE("simple") {
         d = Later<double>(a);
         CHECK(d.get() == Approx(a));
         a = 5.2;
         CHECK(d.get() == Approx(a));
     }
+    SUBCASE("reassigning") {
+        d = (Later<double>)c+b;
+        CHECK(d.get() == Approx(c+b));
+        c.d = 3.33;
+        CHECK(d.get() == Approx(c+b));
+        double ha = 1;
+        d = d - ha;
+        CHECK(d.get() == Approx(c+b-ha));
+    }
+}
+TEST_CASE("tool-libs: later: in container") {
+    double a{3.4}, b{5.4};
+    struct Container {
+        Later<double> d;
+    } c {.d = (Later<double>)a};
+    CHECK(c.d.get() == Approx(a));
+    c.d = (Later<double>)b - a;
+    CHECK(c.d.get() == Approx(b-a));
 }
