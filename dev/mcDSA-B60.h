92a326713dc434fcc67f6929ac58a56cdb895669
Jonathan Halmen <slowjo@halmen.xyz>
2023-10-23 12:24:59 +0200

    start implementing mcDSA motor driver

---
+++ dev/mcDSA-B60.h
@@ -0,0 +1,180 @@
+/** @file mcDSA-B60.h
+ *
+ * Copyright (c) 2019 IACE
+ */
+#pragma once
+
+#include <cmath>
+#include <comm/canopen.h>
+
+/**
+ * Represents the side of motor driver
+ */
+enum MotorSide {
+    MotorSideLeft = 0,          ///< left side
+    MotorSideRight = 1          ///< right side
+};
+
+
+/**
+ * @brief General description of a motor driver
+ */
+struct mcDSA : CAN::Open::Device {
+    mcDSA(Sink<CAN::Open::Request> &out, uint8_t id, bool invert) : CAN::Open::Device(out, id) {
+        init(invert);
+        Device::enable(RPDO{
+            .N = 1,
+            .type = RPDO::CHANGE,
+            .map = {{.ix = 0x3300, .sub = 0, .len = 32}},
+            });
+    }
+
+    /** Enable or Disable the motor driver */
+    void enable(bool en) {
+        w8(0x3004, 0, en);
+    }
+
+    //TODO:
+    /** Set the motor speed in u/min.
+     * Capped at +- 1m/s
+     */
+    void speed(double speed) {
+        speed = fmin(1, fmax(-1, speed));
+        /* int iSpeed = (int) (6000 / M_PI / CAR_WHEEL_DIAMETER * dSpeed); // calculates from m/s to (U/100) / min */
+        int iSpeed;
+        w32(0x3300, 0, iSpeed);
+    }
+
+    /**
+     * Read current motor data.
+     *
+     * Includes actual speed, desired speed, motor current,
+     * motor position (angle), motor error.
+     *
+     * Data can be accessed in @ref tData
+     */
+    void readData() {
+        read(0x3a04, 1);//, &this->tData.tSpeed);
+        read(0x3361, 0);//, &this->tData.tSpeedDes);
+        read(0x3262, 1);//, &this->tData.tCurrent);
+        read(0x394a, 0);//, &this->tData.tPosition);
+        read(0x3001, 0);//, &this->tData.tError);
+    }
+
+    /**
+     * Return the current motor speed in m/s
+     */
+    double getSpeed() {
+        return{};
+        /* return ((int) this->tData.tSpeed.u32) * M_PI * CAR_WHEEL_DIAMETER / 6000;       // calculates from (U/100)/min to m/s */
+    }
+
+    /**
+     * Return the current desired motor speed in m/s
+     */
+    double getSpeedDes() {
+        return{};
+        /* return ((int) this->tData.tSpeedDes.u32) * M_PI * CAR_WHEEL_DIAMETER / 6000;       // calculates from (U/100)/min to m/s */
+    }
+
+    /**
+     * Return the current motor current in mA
+     */
+    double getCurrent() {
+        return{};
+        /* return ((int) this->tData.tCurrent.u32); */
+    }
+
+    /**
+     * Set the maximum Acceleration
+     * TODO
+     * @param dAccel in m/s^2
+     */
+    void setMaxAccel(double dAccel) {
+
+    }
+
+    /**
+     * Set the maximum Deceleration
+     * TODO
+     * @param dDecel in m/s^2
+     */
+    void setMaxDecel(double dDecel) {
+
+    }
+    void callback(Request rq) {}
+
+    //TODO: this needs to happen on a higher level: heartbeat
+    //   out.push({.data = 0x5, .id = 0x701, .opts = {.dlc = 1}}); // send heartbeat
+
+    /**
+     * Struct containing the data collected from motor driver
+     */
+    /* struct _data{ */
+    /*     canData tSpeed;     ///< current measured speed */
+    /*     canData tSpeedDes;  ///< current desired speed */
+    /*     canData tCurrent;   ///< current measured motor current */
+    /*     canData tPosition;  ///< current measured motor position */
+    /*     canData tError;     ///< current motor error state */
+    /* } tData;                ///< instance of motor driver data */
+    void init(bool invert) {
+		w8(0x3004, 0, 0); // lock motor driver
+		w8(0x3000, 0, 1); // clear errors
+		//w32(0x3000, 0, 0x82); // reset to defaults
+
+        // motor config
+		w8(0x3900, 0, 1); // motor type: BLDC
+		w16(0x3902, 0, 24000); // nominal supply voltage
+		w8(0x3910, 0, 8); // number of poles
+		w8(0x3911, 0, 1 << (invert ? 0 : 1)); // in case direction needs changing
+		w32(0x3221, 0, 1785); // max positive current
+		w32(0x3223, 0, 1785); // max negative current
+
+        w16(0x3a02, 0, 5); // velocity measure time [ms]
+
+        // current controller
+		w16(0x3250, 0, 0x113); // current feedback through internal sensor
+		w16(0x3210, 0, 200); // Kp
+		w16(0x3211, 0, 100); // Ki
+		w16(0x3219, 1, 256); // Kdiv
+
+        // svel controller settings
+		w32(0x3550, 0, 0x94a); // svel feedback with hall sensors
+		w16(0x3510, 0, 10); // Kp
+		w16(0x3511, 0, 10); // Ki
+		w16(0x35a1, 0, 10000); // svel max drehzahlbereich
+
+        // vel controller settings
+		w16(0x3310, 0, 0); // Kp
+		w16(0x3311, 0, 0); // Ki
+		w16(0x3312, 0, 0); // Kd
+		w16(0x3314, 0, 1000); // feed forward in o/oo
+		w16(0x3315, 0, 0); // acceleration feed forward
+		w32(0x3350, 0, 0x94a); // velocity feed back through HALL sensors
+
+        // vel settings
+		w16(0x33a0, 0, 1000); // sampling time in us
+		w8(0x334c, 0, 1); // turn on ramp generator
+		w32(0x3340, 0, 1000); // ramp generator: vel_acc_dv
+		w32(0x3341, 0, 1000); // ramp generator: vel_acc_dt
+		w32(0x3342, 0, 1000); // ramp generator: vel_dec_dv
+		w32(0x3343, 0, 1000); // ramp generator: vel_dec_dt
+
+        // factors
+		w8(0x3b00, 0, 3); // activate factors
+		w8(0x3b00, 1, 1); // high precision
+		w32(0x3302, 0, 1); // Fvscale zähler
+		w32(0x3303, 0, 100); // Fvscale nenner
+		w32(0x330A, 0, 878); // Fvdim zähler
+		w32(0x330B, 0, 10); // Fvdim nenner
+		w32(0x3B19, 0, 878); // gear motor numerator
+		w32(0x3B19, 1, 10); // gear motor denominator
+
+        //heartbeat settings
+		/* w32(0x1016, 1, (1<<16) | ((int)EXP_DT)); // heartbeat guarding (node | time [ms]) */
+		w8(0x1029, 1, 0); // reaction to error
+        // motor controller startup settings
+		w8(0x3003, 0, 3); // motor driver mode
+		w8(0x3004, 0, 1); // release motor driver
+    }
+};
