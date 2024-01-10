1f4595ef561f6609b932e70e5cff32da97f7a5f2
Jens Wurm <jens.wurm@umit.at>
2024-01-10 19:45:55 +0100

    First try of smooth traj

--- ctrl/trajectory.h
+++ ctrl/trajectory.h
@@ -110,45 +110,40 @@ public:
     }
 };
 
-/* class SmoothTrajectory : public Curve { */
-/*     struct Vec { */
-/*         double x, y; */
-/*     }; */
-/*     Buffer<Vec> P; */
-/* public: */
-/*     void setData(Buffer<double> &&b) override { */
-/*         size_t off{b.size/2}; */
-/*         P = Buffer<Vec>(off); */
-/*         P.len = off; */
-/*         double slope; */
-/*         for (size_t i = 0; i < off; ++i) { */
-/*             if (i+1 < off) { */
-/*                 slope = (b[i+1+off] - b[i+off]) / (b[i+1] - b[i]); */
-/*             } else { */
-/*                 slope = 0; */
-/*             } */
-/*             P[i] = {b[i], b[i+off], slope}; */
-/*         } */
-/*     } */
-/*     double getValue(double dx) override { */
-/*         if (!P.size) return 0; */
-/*         size_t i = (P.size - 1) / 2; */
-/*         size_t left = 0, right = P.size - 1; */
-/*         while (true) { */
-/*             if (left == right) { */
-/*                 return P[right].y; */
-/*             } */
-/*             if (dx < P[i].x) { */
-/*                 right = i; */
-/*                 i = (left + right) / 2; */
-/*                 continue; */
-/*             } */
-/*             if (dx >= P[i+1].x) { */
-/*                 left = i+1; */
-/*                 i = (left + right) / 2; */
-/*                 continue; */
-/*             } */
-/*             return P[i].m * (dx - P[i].x) + P[i].y; */
-/*         } */
-/*     } */
-/* }; */
+class SmoothTrajectory : public Curve {
+    size_t n, len;
+    double *coeffs;
+    struct {
+        double x0, x1;
+        double y0, y1;
+    } cfg;
+public:
+    SmoothTrajectory(std::initializer_list<double> coeffs, size_t n) : coeffs{new double [n]}, len(0), n(n){
+        for (auto v: coeffs)
+            this->coeffs[len++] = v;
+    }
+    void setData(Buffer<double> &&b) override {
+        assert(b.size > 4);
+        cfg = {b[0], b[2], b[1], b[3]};
+    }
+    double getValue(double x) override {
+        if (x < cfg.x0)
+            return cfg.y0;
+        else if (x > cfg.x1)
+            return cfg.y1;
+        else {
+            double dy = cfg.y1 - cfg.y0;
+            double tau = (x - cfg.x0) / (cfg.x1 - cfg.x0);
+
+            return cfg.y0 + dy * polyVal(tau);
+        }
+    }
+
+    double polyVal(double dx) {
+        double res = 0;
+        for (int i = n; i >= 0; i --) {
+            res = res * dx + coeffs[i - len];
+        }
+        return res;
+    }
+};
