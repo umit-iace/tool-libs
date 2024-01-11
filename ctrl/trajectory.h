721bed20a54221579b292418c8388bb8b21c1b06
Jens Wurm <jens.wurm@umit.at>
2024-01-11 22:32:05 +0100

    Fix scaling in smooth, remove step, update return type

--- ctrl/trajectory.h
+++ ctrl/trajectory.h
@@ -6,73 +6,47 @@
 #include <utility>
 #include <utils/buffer.h>
 
+
 /** Parametrized Curve
  *
  * useful for trajectory generation
  **/
 struct Curve {
+    Buffer<double> diffs;
 public:
     virtual void setData(Buffer<double> &&b) {
         assert(false); // ups, virtual call didn't work?
     }
     /** virtual destructor */
     virtual ~Curve() {}
+    Curve() : diffs(2) {
+        for (int i = 0; i < diffs.size; ++i) diffs.append(0);
+    }
+    Curve(size_t n) : diffs(n) {
+        for (int i = 0; i < diffs.size; ++i) diffs.append(0);
+    }
 
-    double operator()(double dx) {
+    Buffer<double>  operator()(double dx) {
         return getValue(dx);
     }
 
-    virtual double getValue(double) {
+    virtual Buffer<double>  getValue(double) {
         assert(false); // ups, virtual call didn't work?
         return 0;
     }
 };
 
-class StepTrajectory : public Curve {
-    struct Vec {
-        double x, y;
-    };
-    Buffer<Vec> P;
-public:
-    void setData(Buffer<double> &&b) override {
-        size_t i{0}, off{b.size/2};
-        P = Buffer<Vec>(off);
-        P.len = off;
-        for (auto &v : P) {
-            v = {b[i], b[i+off]};
-            ++i;
-        }
-    }
-    double getValue(double dx) override {
-        if (!P.size) return 0;
-        size_t i = (P.size - 1) / 2;
-        size_t left = 0, right = P.size - 1;
-        while (true) {
-            if (left == right) {
-                return P[right].y;
-            }
-            if (dx < P[i].x) {
-                right = i;
-                i = (left + right) / 2;
-                continue;
-            }
-            if (dx >= P[i+1].x) {
-                left = i+1;
-                i = (left + right) / 2;
-                continue;
-            }
-            return P[i].y;
-        }
-    }
-};
-
 /** Class implementing linear interpolation */
-class LinearTrajectory : public Curve {
+class LinearTrajectory : Curve {
+    Buffer<double> diffs;
     struct Vec {
         double x, y, m;
     };
     Buffer<Vec> P;
 public:
+    LinearTrajectory() : diffs(2) {}
+    LinearTrajectory(size_t diffs) : diffs(diffs) {}
+
     void setData(Buffer<double> &&b) override {
         size_t off{b.size/2};
         P = Buffer<Vec>(off);
@@ -87,13 +61,18 @@ public:
             P[i] = {b[i], b[i+off], slope};
         }
     }
-    double getValue(double dx) override {
-        if (!P.size) return 0;
+    Buffer<double> getValue(double dx) override {
+        if (!P.size) {
+            diffs[0] = 0;
+            diffs[1] = 0;
+            return diffs;
+        };
         size_t i = (P.size - 1) / 2;
         size_t left = 0, right = P.size - 1;
         while (true) {
             if (left == right) {
-                return P[right].y;
+                diffs[0] =  P[right].y;
+                diffs[1] = 0;
             }
             if (dx < P[i].x) {
                 right = i;
@@ -105,66 +84,57 @@ public:
                 i = (left + right) / 2;
                 continue;
             }
-            return P[i].m * (dx - P[i].x) + P[i].y;
+            diffs[0] = P[i].m * (dx - P[i].x) + P[i].y;
+            diffs[1] = P[i].m;
+            break;
         }
+        return diffs;
     }
 };
 
-class SmoothTrajectory : public Curve {
-    size_t n, len;
-    double *coeffs;
-    double *diffs;
+/** Class implementing polynom interpolation */
+class SmoothTrajectory : Curve {
+    size_t n;
+    Buffer<double> coeffs;
     struct {
         double x0, x1;
         double y0, y1;
     } cfg;
 public:
-    SmoothTrajectory(std::initializer_list<double> coeffs, size_t n) : coeffs{new double [coeffs.size()]},
-                                                                       diffs{new double [n - coeffs.size()]},
-                                                                       len(0), n(n){
-        for (auto v: coeffs)
-            this->coeffs[len++] = v;
-    }
+    SmoothTrajectory(Buffer<double> coeffs, size_t n) : coeffs{coeffs}, n(n), Curve(coeffs.len) {}
     void setData(Buffer<double> &&b) override {
         assert(b.size <= 4);
         cfg = {b[0], b[1], b[2], b[3]};
     }
-    double getValue(double x) override {
+    Buffer<double> getValue(double x) override {
+        for (int l = 0; l < diffs.len; ++l)  diffs[l] = 0;
+
         if (x < cfg.x0) {
-            return cfg.y0;
+            diffs[0] = cfg.y0;
         } else if (x > cfg.x1) {
-            return cfg.y1;
+            diffs[0] = cfg.y1;
         } else {
             double dy = cfg.y1 - cfg.y0;
             double dx = cfg.x1 - cfg.x0;
             double tau = (x - cfg.x0) / dx;
-            polyVal(tau, dx, dy);
 
-            return cfg.y0 + diffs[0];
-        }
-    }
-
-    double diff(size_t order){
-        return diffs[order];
-    }
-
-    void polyVal(double x, double dx, double dy) {
-        for (int l = 0; l < len; ++l) {
-            diffs[l] = 0;
-        }
-
-        for (int i = n; i >= 0; --i) {
-            for (int j = n - len; j > 0; --j) {
-                diffs[j] = diffs[j] * x + diffs[j - 1] / dx;
+            for (int i = n; i >= 0; --i) {
+                for (int j = n - diffs.len; j > 0; --j) {
+                    diffs[j] = diffs[j] * tau + diffs[j - 1];
+                }
+                if (n - i >= coeffs.len)
+                    diffs[0] *= tau;
+                else
+                    diffs[0] = diffs[0] * tau + coeffs[i - coeffs.len];
             }
-            if (n - i > len)
-                diffs[0] = diffs[0] * x;
-            else
-                diffs[0] = diffs[0] * x + coeffs[i - len];
-        }
 
-        for (int l = 0; l < len; ++l) {
-            diffs[l] *= dy;
+            for (int l = 0; l < diffs.len; ++l) {
+                if (l > 0)
+                    diffs[l] *= dy * l / pow(dx, l);
+                else
+                    diffs[l] = cfg.y0 + diffs[l] * dy;
+            }
         }
+        return diffs;
     }
 };
