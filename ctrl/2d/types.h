0325acaddc955296b46c89fb3553e94932324a5a
Jonathan Halmen <slowjo@halmen.xyz>
2024-01-17 21:09:46 +0100

    constexpr all the things

--- ctrl/2d/types.h
+++ ctrl/2d/types.h
@@ -10,15 +10,15 @@ namespace Ctrl2D {
 /** Position and Orientation of UniCycleModel in 2D space */
 struct Loc {
     double x, y, t;
-    friend Loc operator*(float v, Loc a) { return { v*a.x, v*a.y, v*a.t };}
-    friend Loc operator+(Loc a, Loc b) { return { a.x+b.x, a.y+b.y, a.t+b.t }; }
-    friend Loc operator-(Loc a, Loc b) { return { a.x-b.x, a.y-b.y, a.t-b.t }; }
+    constexpr friend Loc operator*(float v, Loc a) { return { v*a.x, v*a.y, v*a.t };}
+    constexpr friend Loc operator+(Loc a, Loc b) { return { a.x+b.x, a.y+b.y, a.t+b.t }; }
+    constexpr friend Loc operator-(Loc a, Loc b) { return { a.x-b.x, a.y-b.y, a.t-b.t }; }
 };
 
 /** reference position + derivatives */
 struct Reference {
     double x[4], y[4];
-    friend Reference operator+(Reference a, Reference b) { return {
+    constexpr friend Reference operator+(Reference a, Reference b) { return {
         .x = {
             a.x[0] + b.x[0],
             a.x[1] + b.x[1],
@@ -31,7 +31,7 @@ struct Reference {
             a.y[3] + b.y[3],
         }};
     }
-    friend Reference operator-(Reference a, Reference b) { return {
+    constexpr friend Reference operator-(Reference a, Reference b) { return {
         .x = {
             a.x[0] - b.x[0],
             a.x[1] - b.x[1],
@@ -49,7 +49,7 @@ struct Reference {
 /** reference in Invariant coordinates */
 struct Invariant {
     double tau[3], nu[3], theta[3];
-    friend Invariant operator+(Invariant a, Invariant b) { return {
+    constexpr friend Invariant operator+(Invariant a, Invariant b) { return {
         .tau = {
             a.tau[0] + b.tau[0],
             a.tau[1] + b.tau[1],
@@ -64,7 +64,7 @@ struct Invariant {
             a.theta[2] + b.theta[2],
         }};
     }
-    friend Invariant operator-(Invariant a, Invariant b) { return {
+    constexpr friend Invariant operator-(Invariant a, Invariant b) { return {
         .tau = {
             a.tau[0] - b.tau[0],
             a.tau[1] - b.tau[1],
@@ -84,17 +84,17 @@ struct Invariant {
 /** in m/s */
 struct Wheels {
     double l, r;
-    friend Wheels operator*(float v, Wheels w) { return { v*w.l, v*w.r };}
-    friend Wheels operator+(Wheels a, Wheels b) { return { a.l+b.l, a.r+b.r };}
-    friend Wheels operator-(Wheels a, Wheels b) { return { a.l-b.l, a.r-b.r };}
+    constexpr friend Wheels operator*(float v, Wheels w) { return { v*w.l, v*w.r };}
+    constexpr friend Wheels operator+(Wheels a, Wheels b) { return { a.l+b.l, a.r+b.r };}
+    constexpr friend Wheels operator-(Wheels a, Wheels b) { return { a.l-b.l, a.r-b.r };}
 };
 
 /** in m/s, rad/s */
 struct Input {
     double v, w;
-    friend Input operator*(float v, Input o) { return { v*o.v, v*o.w };}
-    friend Input operator+(Input a, Input b) { return { a.v+b.v, a.w+b.w };}
-    friend Input operator-(Input a, Input b) { return { a.v-b.v, a.w-b.w };}
+    constexpr friend Input operator*(float v, Input o) { return { v*o.v, v*o.w };}
+    constexpr friend Input operator+(Input a, Input b) { return { a.v+b.v, a.w+b.w };}
+    constexpr friend Input operator-(Input a, Input b) { return { a.v-b.v, a.w-b.w };}
 };
 
 /** generate invariant coordinates from cartesian reference */
