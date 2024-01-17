778e67f2bde25b97320177def65cacc2849231fb
Jens Wurm <jens.wurm@umit.at>
2024-01-17 12:42:43 +0100

    Extend smooth traj to multiple x,y points

--- ctrl/trajectory.h
+++ ctrl/trajectory.h
@@ -97,45 +97,65 @@ public:
 class SmoothTrajectory : public Curve {
     size_t n;
     Buffer<double> coeffs;
-    struct {
-        double x0, x1;
-        double y0, y1;
-    } cfg;
+    struct Vec {
+        double x, y;
+    };
+    Buffer<Vec> P;
 public:
     SmoothTrajectory(Buffer<double> coeffs, size_t n) : coeffs{coeffs}, n(n), Curve(coeffs.len) {}
     void setData(Buffer<double> &&b) override {
-        assert(b.size == 4);
-        cfg = {b[0], b[1], b[2], b[3]};
+        size_t off{b.size/2};
+        P = Buffer<Vec>(off);
+        P.len = off;
+        double slope;
+        for (size_t i = 0; i < off; ++i) {
+            P[i] = {b[i], b[i+off]};
+        }
     }
     Buffer<double> getValue(double x) override {
         for (int l = 0; l < diffs.len; ++l)  diffs[l] = 0;
 
-        if (x < cfg.x0) {
-            diffs[0] = cfg.y0;
-        } else if (x > cfg.x1) {
-            diffs[0] = cfg.y1;
-        } else {
-            double dy = cfg.y1 - cfg.y0;
-            double dx = cfg.x1 - cfg.x0;
-            double tau = (x - cfg.x0) / dx;
+        size_t i = (P.size - 1) / 2;
+        size_t left = 0, right = P.size - 1;
+        while (true) {
+            if (left == right) {
+                diffs[0] =  P[right].y;
+                diffs[1] = 0;
+                break;
+            }
+            if (x < P[i].x) {
+                right = i;
+                i = (left + right) / 2;
+                continue;
+            }
+            if (x >= P[i+1].x) {
+                left = i+1;
+                i = (left + right) / 2;
+                continue;
+            }
+            double dy = P[i+1].y - P[i].y;
+            double dx = P[i+1].x - P[i].x;
+            double tau = (x - P[i].x) / dx;
 
-            for (int i = n; i >= 0; --i) {
+            for (int k = n; k >= 0; --k) {
                 for (int j = n - diffs.len; j > 0; --j) {
                     diffs[j] = diffs[j] * tau + diffs[j - 1];
                 }
-                if (n - i >= coeffs.len)
+                if (n - k >= coeffs.len)
                     diffs[0] *= tau;
                 else
-                    diffs[0] = diffs[0] * tau + coeffs[i - coeffs.len];
+                    diffs[0] = diffs[0] * tau + coeffs[k - coeffs.len];
             }
 
             for (int l = 0; l < diffs.len; ++l) {
                 if (l > 0)
                     diffs[l] *= dy * l / pow(dx, l);
                 else
-                    diffs[l] = cfg.y0 + diffs[l] * dy;
+                    diffs[l] = P[i].y + diffs[l] * dy;
             }
+            break;
         }
+
         return diffs;
     }
 };
