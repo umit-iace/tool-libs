9e65b88aa377d9f6b4eeb19bda65b13d41a50880
Jonathan Halmen <slowjo@halmen.xyz>
2024-05-10 15:55:50 +0200

    remove dependency on `ncat`, deprecate sys/host.h
    removes special treatment of the linux backend -- not all programs need
    both the UDP and TTY interfaces -- they shall declare and use only what
    they need.
    
    also gets rid of the `ncat` dependency, _and_ -- equally important --
    the need to run in multiple threads. This still did not allow cleaning
    up the program gracefully and running all destructors.
    
    The UDP implementation arguably works better than communicating with
    `ncat` -- no need to wait for `ncat` to restart after the 'connection
    drops' -- UDP is connectionless, this implementation reflects that.
    
    `sys/host.h` is still available for now, implementing the old `host`
    struct on top of the new backends, but I'm in favor of removing it
    completely.

--- linux/hal.cpp
+++ linux/hal.cpp
@@ -1,4 +1,4 @@
-#include "sys/host.h"
+#include <core/kern.h>
 
 #include <chrono>
 #include <cstdio>
@@ -35,102 +35,12 @@ void sighandler(int signum) {
     }
 }
 
-struct impl {
-    struct Pipe {
-        union {
-            struct {
-                int read, write;
-            };
-            int fd[2];
-        };
-        Pipe() {
-            pipe(fd);
-        }
-    } p_in{}, p_out{};
-    int c_pid {fork()};
-    impl() {
-        if (!c_pid) {
-            // make child communicate with parent through pipes
-            close(STDIN_FILENO); close(STDOUT_FILENO);
-            dup2(p_in.write, STDOUT_FILENO);
-            dup2(p_out.read, STDIN_FILENO);
-            close(p_in.read); close(p_in.write);
-            close(p_out.read); close(p_out.write);
-            int ret = 0;
-            while (ret != 0x7f00) {
-                fprintf(stderr, "starting ncat\n");
-                ret = system("ncat -lui1 127.0.0.1 45670");
-            }
-            exit(EXIT_FAILURE);
-        }
-        close(p_in.write); close(p_out.read);
+bool hal = [](){ // initialize signal handlers
         struct sigaction act{};
         act.sa_handler = sighandler;
         sigfillset(&act.sa_mask);
         sigaction(SIGINT, &act, NULL);
         sigaction(SIGSEGV, &act, NULL);
-    }
-    ~impl() {
-        if (c_pid) {
-            kill(c_pid, 15);
-            int stat;
-            waitpid(c_pid, &stat, 0);
-        }
-    }
-    struct READ: Source<Buffer<uint8_t>> {
-        Queue<Buffer<uint8_t>> q{30};
-        Buffer<uint8_t> b = 512;
-        const int fd, pid;
-        READ(int fd, int pid) : fd(fd), pid(pid) { }
-        bool empty() override {
-            int stat = 0;
-            if (pid == waitpid(pid, &stat, WNOHANG) && WIFEXITED(stat))
-                exit(EXIT_FAILURE);
-            struct pollfd fds[1];
-            int ret{};
-            fds[0].fd = fd;
-            fds[0].events = POLLIN;
-            ssize_t l{};
-            do {
-                ret = poll(fds, 1, 0);
-                if (ret <= 0) break;
-                l = read(fd, b.buf, b.size);
-                if (l == -1) break;
-                b.len = l;
-                if (l) {
-                    q.push(std::move(b));
-                    b = 512;
-                }
-            } while (l == b.size);
-            return q.empty();
-        }
-        Buffer<uint8_t> pop() override {
-            return q.pop();
-        }
-    };
-    struct WRITE: Sink<Buffer<uint8_t>> {
-        const int fd{};
-        WRITE(int fd) : fd{fd} { }
-        void push(Buffer<uint8_t> &&b) override {
-            size_t off{};
-            do {
-                off += write(fd, b.buf+off, b.len-off);
-            } while (off < b.len);
-        }
-    };
+        return true;
+}();
 
-    READ readSock{p_in.read, c_pid}, readTTY{STDIN_FILENO, c_pid};
-    WRITE writeSock{p_out.write}, writeTTY{STDOUT_FILENO};
-} impl;
-
-
-Host host{
-    .socket{
-        .in = impl.readSock,
-        .out = impl.writeSock,
-    },
-    .tty{
-        .in = impl.readTTY,
-        .out = impl.writeTTY,
-    },
-};
--- linux/sys/host.h
+++ linux/sys/host.h
@@ -1,11 +1,24 @@
 #pragma once
-#include <core/kern.h>
-#include <core/streams.h>
-#include <cstdlib>
-extern struct Host {
+#warning usage of <sys/host.h> has been deprecated.
+#warning make use of <sys/comm.h> and/or <sys/sim.h> instead
+#include "comm.h"
+#include "sim.h"
+
+inline TTY tty {"/dev/tty"};
+inline UDP udp {"127.0.0.1", 45670};
+inline struct Host {
     struct {
         Source<Buffer<uint8_t>> &in;
         Sink<Buffer<uint8_t>> &out;
     } socket, tty;
-} host;
+} host {
+    .socket = {
+        .in = udp,
+        .out = udp,
+    },
+    .tty = {
+        .in = tty,
+        .out = tty,
+    },
+};
 
