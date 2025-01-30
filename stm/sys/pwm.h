46a2d00ba439cdb2037ac20f924de3c4696df939
Jonathan Halmen <slowjo@halmen.xyz>
2025-01-30 13:22:58 +0100

    stm/pwm: add freq output by changing timer period

--- stm/sys/pwm.h
+++ stm/sys/pwm.h
@@ -20,7 +20,7 @@ namespace TIMER {
          */
         void pwm(double perc) {
             _perc = perc;
-            _ticks = perc * period;
+            _ticks = perc * _period;
             __HAL_TIM_SET_COMPARE(&hTim, chan, _ticks);
         }
 
@@ -29,9 +29,31 @@ namespace TIMER {
          * @param ticks [0..TIMER::HW period]
          */
         void ticks(uint32_t ticks) {
-            _perc = ticks / period;
+            _perc = ticks / _period;
             _ticks = ticks;
             __HAL_TIM_SET_COMPARE(&hTim, chan, _ticks);
+        }
+
+        /**
+         * change frequency by setting period of timer
+         * freq = base / period
+         * counter is blocked while period = 0
+         * @param period []
+         */
+        void period(uint16_t period) {
+            if (period == _period) return;
+            bool needupdate = _period == 0;
+            _period = period;
+            hTim.Instance->CR1 |= TIM_CR1_UDIS;
+            hTim.Instance->ARR = _period;
+            pwm(_perc);
+            hTim.Instance->CR1 &= ~TIM_CR1_UDIS;
+            if (needupdate || hTim.Instance->CNT >= _period)
+                hTim.Instance->EGR |= TIM_EGR_UG;
+        }
+
+        uint16_t period() {
+            return _period;
         }
 
         /** PWM configuration */
@@ -45,7 +67,7 @@ namespace TIMER {
          * Initialize Timer Channel
          */
         PWM(const Config &conf)
-                : hTim(conf.tim->handle), chan(conf.chan), period(hTim.Init.Period) {
+                : hTim(conf.tim->handle), chan(conf.chan), _period(hTim.Init.Period) {
             // pwm config of timer
             while (HAL_TIM_PWM_Init(&hTim) != HAL_OK);
 
@@ -62,18 +84,24 @@ namespace TIMER {
         }
 
         /** get currently set pwm value in percent */
-        double getpwm(){
+        double pwm(){
             return _perc;
         }
+        double getpwm(){
+            return pwm();
+        }
         /** get currently set pwm value in timer ticks */
-        uint32_t getticks(){
+        uint32_t ticks(){
             return _ticks;
+        }
+        uint32_t getticks(){
+            return ticks();
         }
 
     private:
-        TIM_HandleTypeDef hTim{};
+        TIM_HandleTypeDef &hTim;
         uint32_t chan = 0;
-        uint32_t period = 0;
+        uint32_t &_period;
         double _perc = 0.0;
         uint32_t _ticks = 0;
     };
